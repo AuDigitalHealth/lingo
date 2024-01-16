cc21e5bb300eaf45dcad89747ce70692dd18c72b
Clinton Gillespie <clinton.gillespie@csiro.au>
2024-01-16 10:42:27 +1000

    moved some strings that were used in multiple places - cache - to CacheConstants.java

--- api/src/main/java/com/csiro/snomio/configuration/CachingConfig.java
+++ api/src/main/java/com/csiro/snomio/configuration/CachingConfig.java
@@ -1,5 +1,6 @@
 package com.csiro.snomio.configuration;
 
+import com.csiro.snomio.util.CacheConstants;
 import lombok.extern.java.Log;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,19 +14,19 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Log
 public class CachingConfig {
 
-  @CacheEvict(value = "users", allEntries = true)
+  @CacheEvict(value = CacheConstants.USERS, allEntries = true)
   @Scheduled(fixedRateString = "${caching.spring.usersTTL}")
   public void emptyUsersCache() {
     log.info("emptying user cache");
   }
 
-  @CacheEvict(value = "snowstorm-status", allEntries = true)
+  @CacheEvict(value = CacheConstants.SNOWSTORM_STATUS, allEntries = true)
   @Scheduled(fixedRateString = "60000")
   public void emptySnowstormStatusCache() {
     log.info("Emptying snowstorm status cache");
   }
 
-  @CacheEvict(value = "ap-status", allEntries = true)
+  @CacheEvict(value = CacheConstants.AP_STATUS, allEntries = true)
   @Scheduled(fixedRateString = "60000")
   public void emptyApStatusCache() {
     log.info("Emptying ap status cache");
---
+++ api/src/main/java/com/csiro/snomio/util/CacheConstants.java
@@ -0,0 +1,8 @@
+package com.csiro.snomio.util;
+
+public class CacheConstants {
+
+  public static final String USERS = "users";
+  public static final String SNOWSTORM_STATUS = "snowstorm-status";
+  public static final String AP_STATUS = "ap-status";
+}
