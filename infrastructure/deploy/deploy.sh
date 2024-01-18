0fa478cb6cd4132b3c1a8f4089a4e4a3e2aae919
Clinton Gillespie <clinton.gillespie@csiro.au>
2024-01-18 14:59:48 +1000

    move ims-username/password to deploy.sh

--- infrastructure/deploy/deploy.sh
+++ infrastructure/deploy/deploy.sh
@@ -7,6 +7,8 @@ fi
 
 SNOMIO_ENV=${SNOMIO_ENV:-dev}
 DB_USER=${DB_USER:snomioapi_dev}
+IMS_USERNAME=${}
+IMS_PASSWORD=${}
 KUBECONFIG=${KUBECONFIG:-~/.kube/config}
 SNOMIO_IMAGE_TAG=${SNOMIO_IMAGE_TAG:-snomio-20230824.1_main}
 HELM_LOCATION=${HELM_LOCATION:-./helm}
@@ -15,6 +17,8 @@ AZ_ACCOUNTKEY=${AZ_ACCOUNTKEY:-settheenvvar}
 helm upgrade --install --kubeconfig ${KUBECONFIG} --namespace snomio-${SNOMIO_ENV} --values snomio-${SNOMIO_ENV}.yaml \
   --set snomio.image="nctsacr.azurecr.io/snomio:${SNOMIO_IMAGE_TAG}" \
   --set snomio.config."spring\.datasource\.username"=${DB_USER} \
+  --set snomio.config."ims-username"=${IMS_USERNAME} \
+  --set  snomio.config."ims-password"=${IMS_PASSWORD} \
   --set snomio.database.password="${DB_PASSWORD}" \
   --set snomio.attachments.store.azaccountkey="${AZ_ACCOUNTKEY}" \
   --wait --create-namespace snomio-${SNOMIO_ENV} ${HELM_LOCATION}
