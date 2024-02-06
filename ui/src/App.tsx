11b90d912b9869c8ca0db4375105e0acb8de504c
Clinton Gillespie <clinton.gillespie@csiro.au>
2024-02-06 15:48:01 +1000

    removed commented code

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,14 @@
 // import Router from './router/Router';
-import Router, { browserRouter } from './router';
+import { browserRouter } from './router';
 import './app.css';
 import { store } from './store';
 import { Provider as ReduxProvider } from 'react-redux';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
   return (
     <ReduxProvider store={store}>
-      {/* <BrowserRouter> */}
-      {/* <Router /> */}
       <RouterProvider router={browserRouter} />
-      {/* </BrowserRouter> */}
     </ReduxProvider>
   );
 }
