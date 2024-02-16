4a96a12c73c59f9df32a0b8fd8a7ef84db04616e
Attila Edelenyi <ede_ati@hotmail.com>
2024-02-16 15:50:49 +1000

    Add custom JSON comparator to ignore date and other fields in the compare

--- api/src/test/java/com/csiro/tickets/controllers/ScheduleControllerTest.java
+++ api/src/test/java/com/csiro/tickets/controllers/ScheduleControllerTest.java
@@ -8,8 +8,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.restassured.http.ContentType;
 import java.util.List;
+import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.Customization;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class ScheduleControllerTest extends TicketTestBaseLocal {
@@ -17,6 +22,13 @@ class ScheduleControllerTest extends TicketTestBaseLocal {
   @Autowired LabelRepository labelRepository;
   static final String NEWSCHED = "S1000TEST";
   static final String NEWSCHED_DESC = "This is a Test Schedule";
+  final CustomComparator comparator =
+      new CustomComparator(
+          JSONCompareMode.LENIENT,
+          new Customization("id", (o1, o2) -> true),
+          new Customization("name", (o1, o2) -> true),
+          new Customization("description", (o1, o2) -> true),
+          new Customization("grouping", (o1, o2) -> true));
 
   @Test
   void testListSchedules() {
@@ -41,7 +53,7 @@ class ScheduleControllerTest extends TicketTestBaseLocal {
   }
 
   @Test
-  void testUpdateSchedule() throws JsonProcessingException {
+  void testUpdateSchedule() throws JsonProcessingException, JSONException {
     Schedule schedule = createTestSchedule(NEWSCHED, NEWSCHED_DESC);
     schedule.setDescription(NEWSCHED_DESC + "- Updated");
     schedule.setGrouping(101);
@@ -70,9 +82,10 @@ class ScheduleControllerTest extends TicketTestBaseLocal {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JavaTimeModule());
 
-    Assertions.assertEquals(
+    JSONAssert.assertEquals(
         objectMapper.writeValueAsString(updatedSchedule),
-        objectMapper.writeValueAsString(updatedScheduleFromGet));
+        objectMapper.writeValueAsString(updatedScheduleFromGet),
+        comparator);
     Assertions.assertEquals(NEWSCHED_DESC + "- Updated", updatedSchedule.getDescription());
     Assertions.assertEquals(101, updatedSchedule.getGrouping());
   }
@@ -94,7 +107,7 @@ class ScheduleControllerTest extends TicketTestBaseLocal {
   }
 
   @Test
-  void testGetSchedule() throws JsonProcessingException {
+  void testGetSchedule() throws JsonProcessingException, JSONException {
     Schedule schedule = createTestSchedule(NEWSCHED, NEWSCHED_DESC);
     Schedule scheduleFromGet =
         withAuth()
@@ -107,9 +120,10 @@ class ScheduleControllerTest extends TicketTestBaseLocal {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JavaTimeModule());
 
-    Assertions.assertEquals(
+    JSONAssert.assertEquals(
         objectMapper.writeValueAsString(schedule),
-        objectMapper.writeValueAsString(scheduleFromGet));
+        objectMapper.writeValueAsString(scheduleFromGet),
+        comparator);
     Assertions.assertEquals(NEWSCHED, scheduleFromGet.getName());
     Assertions.assertEquals(NEWSCHED_DESC, scheduleFromGet.getDescription());
     Assertions.assertEquals(100, scheduleFromGet.getGrouping());
