34e3bb59278f3c7b02d0b67123e6e0e02b24e9ac
Attila Edelenyi <ede_ati@hotmail.com>
2024-02-23 12:26:53 +1000

    Add a helper to check the file and do safer logging

---
+++ api/src/main/java/com/csiro/tickets/helper/FileUtils.java
@@ -0,0 +1,35 @@
+package com.csiro.tickets.helper;
+
+import com.csiro.snomio.exception.SnomioProblem;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class FileUtils {
+  public static <T extends SnomioProblem> void checkFile(
+      File originalFile, Class<T> exceptionClass) {
+    T exception;
+    try {
+      exception = exceptionClass.getDeclaredConstructor().newInstance();
+    } catch (InstantiationException
+        | IllegalAccessException
+        | NoSuchMethodException
+        | InvocationTargetException e) {
+      throw new RuntimeException("Error instantiating exception: " + e.getMessage(), e);
+    }
+    try {
+      String canonicalOriginalFilePath = originalFile.getCanonicalPath();
+      if (canonicalOriginalFilePath.contains("..")) {
+        exception.setDetail("Invalid file path provided. Don't use .. in your file path!");
+        throw exception;
+      }
+    } catch (IOException e) {
+      exception.setDetail("Issue while checking file paths: " + e.getMessage());
+      throw exception;
+    }
+    if (!originalFile.exists()) {
+      exception.setDetail("File doesn't exist: " + originalFile.getAbsolutePath());
+      throw exception;
+    }
+  }
+}
