abc334e43a5a76ae924d52f81f6d9babb6b95b5f
Leonie Dickson <leonie.dickson@csiro.au>
2024-03-15 10:37:14 +1100

    add routes for ecl refset tool, display user tasks

---
+++ ui/src/menu-items/eclRefset.tsx
@@ -0,0 +1,21 @@
+import { NavItemType } from '../types/menu';
+import { FormattedMessage } from 'react-intl';
+
+const eclRefsetTool: NavItemType = {
+  id: 'ecl-refset-tool',
+  title: <FormattedMessage id="ecl-refset-tool" />,
+
+  type: 'group',
+  children: [
+    {
+      id: 'ecl-refset-tool',
+      title: <FormattedMessage id="ecl-refset-tool" />,
+      type: 'item',
+      url: '/dashboard/eclRefsetTool',
+      icon: 'edit_note',
+      tooltip: 'ECL Refset Tool',
+    }
+  ],
+};
+
+export default eclRefsetTool;
