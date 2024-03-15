1e9245ea68ca1debe239c603adb15a2b16362a4f
Clinton Gillespie <clinton.gillespie@csiro.au>
2024-03-15 12:58:25 +1000

    added tests JsonField entity, associated tests and changed the methods for createTicket and updateTicket to also handle accepting JsonFields, added test cases for these as well

--- api/src/test/java/com/csiro/tickets/controllers/TicketControllerContainerTest.java
+++ api/src/test/java/com/csiro/tickets/controllers/TicketControllerContainerTest.java
@@ -1,6 +1,8 @@
 package com.csiro.tickets.controllers;
 
 import com.csiro.tickets.AdditionalFieldValueDto;
+import com.csiro.tickets.JsonFieldDto;
+import com.csiro.tickets.TicketMinimalDto;
 import com.csiro.tickets.TicketTestBaseContainer;
 import com.csiro.tickets.controllers.dto.TicketDto;
 import com.csiro.tickets.helper.AdditionalFieldUtils;
@@ -9,13 +11,20 @@ import com.csiro.tickets.helper.SearchCondition;
 import com.csiro.tickets.helper.SearchConditionBody;
 import com.csiro.tickets.helper.TicketResponse;
 import com.csiro.tickets.models.Iteration;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TicketControllerContainerTest extends TicketTestBaseContainer {
+
+  @Autowired private ObjectMapper mapper;
 
   @Test
   void testSearchTicketBodyPagination() {
@@ -85,35 +94,36 @@ public class TicketControllerContainerTest extends TicketTestBaseContainer {
   }
 
   @Test
-  void createTicketComplex() {
-    String testIteration = JsonReader.readJsonFile("tickets/basic-iteration.json");
+  void createTicketComplex() throws JsonProcessingException {
+
     String testTicket = JsonReader.readJsonFile("tickets/create-complex.json");
+    // add the jsonFieldEntry this way, otherwise create-complex will end up too late and confusing
+    // also makes it easier to test it for the update.
+    String jsonFieldEntry = JsonReader.readJsonFile("tickets/tga-entry-9979.json");
 
-    Iteration iteration =
-        withAuth()
-            .contentType(ContentType.JSON)
-            .when()
-            .body(testIteration)
-            .post(this.getSnomioLocation() + "/api/tickets/iterations")
-            .then()
-            .statusCode(200)
-            .extract()
-            .as(Iteration.class);
+    TicketMinimalDto ticketMinimalDto = mapper.readValue(testTicket, TicketMinimalDto.class);
+    JsonFieldDto jsonFieldDto = JsonFieldDto.builder().name("Tga Entry").build();
+    JsonNode jsonNode = mapper.readValue(jsonFieldEntry, JsonNode.class);
+    jsonFieldDto.setValue(jsonNode);
+    List<JsonFieldDto> jsonFieldDtos = new ArrayList<>();
+    jsonFieldDtos.add(jsonFieldDto);
+    ticketMinimalDto.setJsonFields(jsonFieldDtos);
 
-    System.out.println(iteration.getName());
-
+    findOrCreateIteration();
     // this ticket intentionally has an artgid that doesn't exist in the db.
 
     TicketDto responseTicket =
         withAuth()
             .contentType(ContentType.JSON)
             .when()
-            .body(testTicket)
+            .body(ticketMinimalDto)
             .post(this.getSnomioLocation() + "/api/tickets")
             .then()
             .statusCode(200)
             .extract()
             .as(TicketDto.class);
+
+    Assertions.assertEquals(responseTicket.getJsonFields().size(), 1);
 
     Assertions.assertEquals(
         responseTicket.getTitle(),
@@ -149,5 +159,128 @@ public class TicketControllerContainerTest extends TicketTestBaseContainer {
         AdditionalFieldUtils.getAdditionalFieldValueByTypeName(
             additionalFieldValueDtoSet, "TGAEntryHash"),
         "12345");
+  }
+
+  @Test
+  void updateTicketComplex() throws JsonProcessingException {
+    findOrCreateIteration();
+    TicketMinimalDto ticketMinimalDto =
+        TicketMinimalDto.builder().title("Test").description("test").build();
+    JsonFieldDto jsonFieldDto = JsonFieldDto.builder().name("Tga Entry").build();
+    List<JsonFieldDto> startJsonFields = new ArrayList<>();
+    startJsonFields.add(jsonFieldDto);
+    ticketMinimalDto.setJsonFields(startJsonFields);
+    TicketDto responseTicket =
+        withAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .body(ticketMinimalDto)
+            .post(this.getSnomioLocation() + "/api/tickets")
+            .then()
+            .statusCode(200)
+            .extract()
+            .as(TicketDto.class);
+
+    Assertions.assertEquals(1, responseTicket.getJsonFields().size());
+    JsonFieldDto jsonFieldDtoResponse = responseTicket.getJsonFields().get(0);
+    Assertions.assertNull(jsonFieldDtoResponse.getValue().get("EntryType"));
+    String testTicket = JsonReader.readJsonFile("tickets/create-complex.json");
+    // add the jsonFieldEntry this way, otherwise create-complex will end up too large and confusing
+    // also makes it easier to test it for the update.
+    String jsonFieldEntry = JsonReader.readJsonFile("tickets/tga-entry-9979.json");
+
+    TicketMinimalDto ticketMinimalDtoUpdated = mapper.readValue(testTicket, TicketMinimalDto.class);
+    JsonFieldDto jsonFieldDtoUpdated = responseTicket.getJsonFields().get(0);
+    JsonNode jsonNode = mapper.readValue(jsonFieldEntry, JsonNode.class);
+    jsonFieldDtoUpdated.setValue(jsonNode);
+    List<JsonFieldDto> jsonFieldDtos = new ArrayList<>();
+    jsonFieldDtos.add(jsonFieldDtoUpdated);
+    ticketMinimalDtoUpdated.setJsonFields(jsonFieldDtos);
+
+    // this ticket intentionally has an artgid that doesn't exist in the db.
+
+    TicketDto updateResponseTicket =
+        withAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .body(ticketMinimalDtoUpdated)
+            .put(this.getSnomioLocation() + "/api/tickets/" + responseTicket.getId())
+            .then()
+            .statusCode(200)
+            .extract()
+            .as(TicketDto.class);
+
+    Assertions.assertEquals(updateResponseTicket.getJsonFields().size(), 1);
+
+    JsonFieldDto jsonFieldDtoUpdatedResponse = updateResponseTicket.getJsonFields().get(0);
+    Assertions.assertNotNull(jsonFieldDtoUpdatedResponse.getValue().get("EntryType"));
+    Assertions.assertEquals(
+        updateResponseTicket.getTitle(),
+        "TGA - ARTG ID 9979 ZOFRAN ondansetron (as hydrochloride dihydrate) 4mg tablet blister pack");
+    Assertions.assertEquals(updateResponseTicket.getAssignee(), "cgillespie");
+
+    Assertions.assertEquals(updateResponseTicket.getLabels().size(), 1);
+    Assertions.assertEquals(updateResponseTicket.getLabels().get(0).getName(), "JiraExport");
+
+    Assertions.assertEquals(updateResponseTicket.getState().getLabel(), "Closed");
+    Assertions.assertEquals(updateResponseTicket.getSchedule().getName(), "S4");
+
+    Set<AdditionalFieldValueDto> additionalFieldValueDtoSet =
+        updateResponseTicket.getAdditionalFieldValues();
+    Assertions.assertEquals(updateResponseTicket.getAdditionalFieldValues().size(), 5);
+    Assertions.assertEquals(
+        AdditionalFieldUtils.getAdditionalFieldValueByTypeName(
+            additionalFieldValueDtoSet, "ARTGID"),
+        "69696969");
+    Assertions.assertEquals(
+        AdditionalFieldUtils.getAdditionalFieldValueByTypeName(
+            additionalFieldValueDtoSet, "StartDate"),
+        "1991-04-17T00:00:00.000+10:00");
+    Assertions.assertEquals(
+        AdditionalFieldUtils.getAdditionalFieldValueByTypeName(
+            additionalFieldValueDtoSet, "EffectiveDate"),
+        "2019-12-05T00:00:00.000+10:00");
+    Assertions.assertEquals(
+        AdditionalFieldUtils.getAdditionalFieldValueByTypeName(
+            additionalFieldValueDtoSet, "AMTFlags"),
+        "PBS");
+    Assertions.assertEquals(
+        AdditionalFieldUtils.getAdditionalFieldValueByTypeName(
+            additionalFieldValueDtoSet, "TGAEntryHash"),
+        "12345");
+  }
+
+  private void findOrCreateIteration() throws JsonProcessingException {
+
+    List<Iteration> iterations =
+        withAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .get(this.getSnomioLocation() + "/api/tickets/iterations")
+            .then()
+            .statusCode(200)
+            .extract()
+            .jsonPath()
+            .getList(".", Iteration.class);
+
+    //    List<Iteration> iterationDtos = mapper.readValue(iterationString, new
+    // TypeReference<List<Iteration>>() {});
+
+    if (iterations.size() > 0) {
+      return;
+    }
+
+    String testIteration = JsonReader.readJsonFile("tickets/basic-iteration.json");
+
+    Iteration iteration =
+        withAuth()
+            .contentType(ContentType.JSON)
+            .when()
+            .body(testIteration)
+            .post(this.getSnomioLocation() + "/api/tickets/iterations")
+            .then()
+            .statusCode(200)
+            .extract()
+            .as(Iteration.class);
   }
 }
