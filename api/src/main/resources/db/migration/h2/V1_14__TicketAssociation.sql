dbe92f3b26c36c8097524907b5733db56b0d7f1e
Clinton Gillespie <clinton.gillespie@csiro.au>
2024-03-26 14:33:14 +1000

    sonarcloud fixes

--- api/src/main/resources/db/migration/h2/V1_14__TicketAssociation.sql
+++ api/src/main/resources/db/migration/h2/V1_14__TicketAssociation.sql
@@ -1,7 +1,13 @@
-ALTER table ticket_association drop column ticket_id;
-ALTER table ticket_association ADD COLUMN ticket_source_id bigint references ticket;
-ALTER table ticket_association ADD COLUMN ticket_target_id bigint references ticket;
+alter table ticket_association DROP COLUMN ticket_id;
+alter table ticket_association DROP COLUMN description;
+alter table ticket_association_aud DROP COLUMN ticket_id;
+alter table ticket_association_aud DROP COLUMN description;
 
-ALTER table ticket_association_aud drop column ticket_id;
-ALTER table ticket_association_aud ADD COLUMN ticket_source_id bigint references ticket(id);
-ALTER table ticket_association_aud ADD COLUMN ticket_target_id bigint references ticket(id);
+ALTER TABLE ticket_association
+    ADD COLUMN ticket_source_id bigint REFERENCES ticket;
+ALTER TABLE ticket_association
+    ADD COLUMN ticket_target_id bigint REFERENCES ticket;
+ALTER TABLE ticket_association_aud
+    ADD COLUMN ticket_source_id bigint REFERENCES ticket(id);
+ALTER TABLE ticket_association_aud
+    ADD COLUMN ticket_target_id bigint REFERENCES ticket(id);
