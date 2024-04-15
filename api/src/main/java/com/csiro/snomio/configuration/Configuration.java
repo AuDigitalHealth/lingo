c150268cbdc3aeda355c86fb1cdcb4e0e9a0fcb5
Attila Edelenyi <ede_ati@hotmail.com>
2024-04-15 19:36:36 +1000

    Add opentelemetry-operator related mooifications so we can forward traces

--- api/src/main/java/com/csiro/snomio/configuration/Configuration.java
+++ api/src/main/java/com/csiro/snomio/configuration/Configuration.java
@@ -1,13 +1,12 @@
 package com.csiro.snomio.configuration;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 
 @SpringBootApplication(scanBasePackages = {"com.csiro.snomio", "com.csiro.tickets"})
 @EnablePrometheusEndpoint
