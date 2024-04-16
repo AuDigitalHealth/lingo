b4ee42c4268b4dc1ce381f4d9c52926f770d03cb
Attila Edelenyi <ede_ati@hotmail.com>
2024-04-16 12:17:54 +1000

    Ran prettier

--- ui/src/Tracing.tsx
+++ ui/src/Tracing.tsx
@@ -1,28 +1,33 @@
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { BatchSpanProcessor, WebTracerConfig, WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import {
+  BatchSpanProcessor,
+  WebTracerConfig,
+  WebTracerProvider,
+} from '@opentelemetry/sdk-trace-web';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { Resource } from '@opentelemetry/resources';
 
 export function initializeOpenTelemetry(): void {
-    const resource = new Resource({
-        [SEMRESATTRS_SERVICE_NAME]: "snomio-ui",
-    });
+  const resource = new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: 'snomio-ui',
+  });
 
-    const providerConfig: WebTracerConfig = {
-        resource: resource,
-    };
-    const provider = new WebTracerProvider(providerConfig);
-    const exporter = new OTLPTraceExporter({
-      url: '/api/telemetry',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      concurrencyLimit: 10,
-    });
-    provider.addSpanProcessor(new BatchSpanProcessor(exporter, {
+  const providerConfig: WebTracerConfig = {
+    resource: resource,
+  };
+  const provider = new WebTracerProvider(providerConfig);
+  const exporter = new OTLPTraceExporter({
+    url: '/api/telemetry',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    concurrencyLimit: 10,
+  });
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(exporter, {
       // The maximum queue size. After the size is reached spans are dropped.
       maxQueueSize: 100,
       // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
@@ -31,16 +36,14 @@ export function initializeOpenTelemetry(): void {
       scheduledDelayMillis: 500,
       // How long the export can run before it is cancelled
       exportTimeoutMillis: 30000,
-    }));
+    }),
+  );
 
-    provider.register({
-        contextManager: new ZoneContextManager(),
-      });
+  provider.register({
+    contextManager: new ZoneContextManager(),
+  });
 
-    registerInstrumentations({
-      instrumentations: [
-        getWebAutoInstrumentations(),
-      ],
-    });
-
+  registerInstrumentations({
+    instrumentations: [getWebAutoInstrumentations()],
+  });
 }
