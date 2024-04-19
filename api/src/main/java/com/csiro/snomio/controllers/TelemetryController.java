964e322f76a8368780c9091e67593a173dc32622
Attila Edelenyi <ede_ati@hotmail.com>
2024-04-19 16:06:18 +1000

    Change telemetry endpoint to parse Zipkin format

--- api/src/main/java/com/csiro/snomio/controllers/TelemetryController.java
+++ api/src/main/java/com/csiro/snomio/controllers/TelemetryController.java
@@ -4,9 +4,7 @@ import com.csiro.snomio.auth.model.ImsUser;
 import com.csiro.snomio.exception.TelemetryProblem;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import java.io.IOException;
 import java.util.Base64;
@@ -33,17 +31,15 @@ public class TelemetryController {
   private boolean telemetryEnabled;
 
   public TelemetryController(WebClient.Builder webClientBuilder) {
-    // WebClient.Builder should be configured globally or injected here
     this.webClient = webClientBuilder.build();
   }
 
   @PostMapping("/api/telemetry")
   public Mono<Void> forwardTelemetry(@RequestBody byte[] telemetryData) {
     if (!telemetryEnabled) {
-      return Mono.empty(); // Return empty response if telemetry is disabled
+      return Mono.empty();
     }
 
-    // Example of Data Enrichment: Adding metadata (this is just a placeholder)
     ImsUser imsUser =
         (ImsUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     String user = Base64.getEncoder().encodeToString(imsUser.getLogin().getBytes());
@@ -51,67 +47,34 @@ public class TelemetryController {
 
     return webClient
         .post()
-        .uri(otelExporterEndpoint + "/v1/traces") // Append endpoint to base URL
+        .uri(otelExporterEndpoint + "/api/v2/spans")
         .contentType(MediaType.APPLICATION_JSON)
         .bodyValue(finalData)
         .retrieve()
         .bodyToMono(Void.class)
-        .doOnError(
-            e ->
-                log.severe(
-                    "Failed to forward telemetry data: "
-                        + e.getMessage())); // No need to return any content
+        .doOnError(e -> log.severe("Failed to forward telemetry data: " + e.getMessage()));
   }
 
   @WithSpan
-  private String addExtraInfo(byte[] telemetryData, @SpanAttribute("user") String user) {
+  private String addExtraInfo(byte[] telemetryData, String user) {
     ObjectMapper mapper = new ObjectMapper();
     try {
       JsonNode root = mapper.readTree(telemetryData);
-      processNestedJsonArray(root, "resourceSpans", mapper, user);
+      if (root.isArray()) {
+        for (JsonNode span : root) {
+          addLoginAttribute(span, user);
+        }
+      }
       return mapper.writeValueAsString(root);
     } catch (IOException e) {
       throw new TelemetryProblem("Failed to process telemetry data: " + e.getMessage());
     }
   }
 
-  private void processNestedJsonArray(
-      JsonNode parentNode, String key, ObjectMapper mapper, String user) {
-    if (!parentNode.has(key) || !parentNode.path(key).isArray()) {
-      throw new TelemetryProblem("Invalid or missing '" + key + "' in telemetry data.");
+  private void addLoginAttribute(JsonNode span, String user) {
+    if (span.isObject()) {
+      ObjectNode tags = (ObjectNode) span.path("tags");
+      tags.put("user", user);
     }
-
-    // Process each child node in the array
-    for (JsonNode childNode : parentNode.path(key)) {
-      if (key.equals("spans")) {
-        addLoginAttribute(childNode, mapper, user);
-      } else {
-        processNestedJsonArray(childNode, getNextKey(key), mapper, user);
-      }
-    }
-  }
-
-  private String getNextKey(String currentKey) {
-    switch (currentKey) {
-      case "resourceSpans":
-        return "scopeSpans";
-      case "scopeSpans":
-        return "spans";
-      default:
-        return "";
-    }
-  }
-
-  private void addLoginAttribute(JsonNode span, ObjectMapper mapper, String user) {
-    JsonNode attributesNode = span.path("attributes");
-    if (!attributesNode.isArray()) {
-      throw new TelemetryProblem("Invalid or missing 'attributes' array in span.");
-    }
-
-    ArrayNode attributes = (ArrayNode) attributesNode;
-    ObjectNode userAttribute = mapper.createObjectNode();
-    userAttribute.put("key", "user");
-    userAttribute.set("value", mapper.createObjectNode().put("stringValue", user));
-    attributes.add(userAttribute);
   }
 }
