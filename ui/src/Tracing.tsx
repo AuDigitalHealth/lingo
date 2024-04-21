b52542760e080b3a4ef3a7cd608857a160d354c3
Attila Edelenyi <ede_ati@hotmail.com>
2024-04-21 21:18:43 +1000

    Try to log the response body coming back from the controller to see what the websocket 400s are

--- ui/src/Tracing.tsx
+++ ui/src/Tracing.tsx
@@ -1,5 +1,4 @@
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import {
   BatchSpanProcessor,
   WebTracerConfig,
@@ -9,28 +8,11 @@ import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { Resource } from '@opentelemetry/resources';
 import { B3Propagator, B3InjectEncoding } from '@opentelemetry/propagator-b3';
-import axios, { InternalAxiosRequestConfig, AxiosError } from 'axios';
-import {
-  context,
-  trace,
-  propagation,
-  AttributeValue,
-  Span,
-} from '@opentelemetry/api';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
 import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
 import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
-import Link from './themes/overrides/Link';
-
-interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
-  span?: Span;
-}
-
-interface CustomAxiosError extends AxiosError {
-  config: CustomAxiosRequestConfig;
-}
 
 export function initializeOpenTelemetry(): void {
   const resource = new Resource({
@@ -70,44 +52,4 @@ export function initializeOpenTelemetry(): void {
     ],
   });
 
-  // const tracer = provider.getTracer('snomio-ui');
-
-  // axios.interceptors.request.use(
-  //   (config: CustomAxiosRequestConfig) => {
-  //     const parentSpan = trace.getSpan(context.active());
-  //     const span = tracer.startSpan(`axios_http_request_${config.method}`, {
-  //       attributes: {
-  //         url: config.url,
-  //         'http.method': config.method ?? 'UNKNOWN',
-  //         kind: 'client',
-  //       },
-  //       links: parentSpan ? [{ context: parentSpan.spanContext() }] : [],
-  //     });
-
-  //     Object.keys(config.headers).forEach(key => {
-  //       const headerValue: AttributeValue = String(config.headers[key]);
-  //       span.setAttribute(`http.header.${key}`, headerValue);
-  //     });
-  //     const currentCtx = trace.setSpan(context.active(), span);
-
-  //     propagation.inject(currentCtx, config.headers);
-
-  //     config.span = span;
-  //     return config;
-  //   },
-  //   error => {
-  //     return Promise.reject(error);
-  //   },
-  // );
-
-  // axios.interceptors.response.use(
-  //   response => {
-  //     (response.config as CustomAxiosRequestConfig).span?.end();
-  //     return response;
-  //   },
-  //   (error: CustomAxiosError) => {
-  //     error.config.span?.end();
-  //     return Promise.reject(error);
-  //   },
-  // );
 }
