dff74a47e4219a7409e86efaad15efc6403b79f1
Attila Edelenyi <ede_ati@hotmail.com>
2024-04-24 13:44:03 +1000

    Move the telemetry testing json out to a file

--- api/src/test/java/com/csiro/snomio/controllers/TelemetryControllerTest.java
+++ api/src/test/java/com/csiro/snomio/controllers/TelemetryControllerTest.java
@@ -8,6 +8,8 @@ import com.csiro.snomio.auth.model.ImsUser;
 import com.csiro.snomio.exception.TelemetryProblem;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -47,14 +49,26 @@ class TelemetryControllerTest {
   @Value("${snomio.environment}")
   private String snomioEnvironment;
 
-  private final String OTEL_TELEMETRY_EXAMPLE =
-      "{\"resourceSpans\":[{\"resource\":{\"attributes\":[{\"key\":\"service.name\",\"value\":{\"stringValue\":\"snomio-ui\"}},{\"key\":\"telemetry.sdk.language\",\"value\":{\"stringValue\":\"webjs\"}},{\"key\":\"telemetry.sdk.name\",\"value\":{\"stringValue\":\"opentelemetry\"}},{\"key\":\"telemetry.sdk.version\",\"value\":{\"stringValue\":\"1.23.0\"}}],\"droppedAttributesCount\":0},\"scopeSpans\":[{\"scope\":{\"name\":\"@opentelemetry/instrumentation-xml-http-request\",\"version\":\"0.50.0\"},\"spans\":[{\"traceId\":\"36a11cb339b5957b7666916062140b87\",\"spanId\":\"87df0287278f2b83\",\"name\":\"GET\",\"kind\":3,\"startTimeUnixNano\":\"1713702779032000000\",\"endTimeUnixNano\":\"1713702779033000000\",\"attributes\":[{\"key\":\"http.method\",\"value\":{\"stringValue\":\"GET\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"https://uat-snomio.ihtsdotools.org/authoring-services/authoring-services-websocket/info?t=1713702779020\"}},{\"key\":\"http.status_code\",\"value\":{\"intValue\":0}},{\"key\":\"http.status_text\",\"value\":{\"stringValue\":\"\"}},{\"key\":\"http.host\",\"value\":{\"stringValue\":\"uat-snomio.ihtsdotools.org\"}},{\"key\":\"http.scheme\",\"value\":{\"stringValue\":\"https\"}},{\"key\":\"http.user_agent\",\"value\":{\"stringValue\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36\"}}],\"droppedAttributesCount\":0,\"events\":[{\"attributes\":[],\"name\":\"open\",\"timeUnixNano\":\"1713702779032000000\",\"droppedAttributesCount\":0},{\"attributes\":[],\"name\":\"send\",\"timeUnixNano\":\"1713702779032000000\",\"droppedAttributesCount\":0},{\"attributes\":[],\"name\":\"abort\",\"timeUnixNano\":\"1713702779033000000\",\"droppedAttributesCount\":0}],\"droppedEventsCount\":0,\"status\":{\"code\":0},\"links\":[],\"droppedLinksCount\":0},{\"traceId\":\"164fe794fd608e5e3daa0cbdd6f68278\",\"spanId\":\"1d9cc86c16482ddf\",\"name\":\"GET\",\"kind\":3,\"startTimeUnixNano\":\"1713702839031000000\",\"endTimeUnixNano\":\"1713702839049000000\",\"attributes\":[{\"key\":\"http.method\",\"value\":{\"stringValue\":\"GET\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"https://uat-snomio.ihtsdotools.org/authoring-services/authoring-services-websocket/info?t=1713702839023\"}},{\"key\":\"http.status_code\",\"value\":{\"intValue\":0}},{\"key\":\"http.status_text\",\"value\":{\"stringValue\":\"\"}},{\"key\":\"http.host\",\"value\":{\"stringValue\":\"uat-snomio.ihtsdotools.org\"}},{\"key\":\"http.scheme\",\"value\":{\"stringValue\":\"https\"}},{\"key\":\"http.user_agent\",\"value\":{\"stringValue\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36\"}}],\"droppedAttributesCount\":0,\"events\":[{\"attributes\":[],\"name\":\"open\",\"timeUnixNano\":\"1713702839031000000\",\"droppedAttributesCount\":0},{\"attributes\":[],\"name\":\"send\",\"timeUnixNano\":\"1713702839031100000\",\"droppedAttributesCount\":0},{\"attributes\":[],\"name\":\"abort\",\"timeUnixNano\":\"1713702839049000000\",\"droppedAttributesCount\":0}],\"droppedEventsCount\":0,\"status\":{\"code\":0},\"links\":[],\"droppedLinksCount\":0}]}]}]}";
-  private final String ZIPKIN_TELEMETRY_EXAMPLE =
-      "[{\"traceId\":\"ed2ffe2033f25e7754ec22080f97a8bb\",\"name\":\"axios_http_request_get\",\"id\":\"a42800bcceaa2f32\",\"timestamp\":1713860461394000,\"duration\":548200,\"localEndpoint\":{\"serviceName\":\"snomio-ui\"},\"tags\":{\"url\":\"/api/status\",\"http.method\":\"get\",\"kind\":\"client\",\"http.header.Accept\":\"application/json, text/plain, */*\",\"http.header.Content-Type\":\"undefined\",\"service.name\":\"snomio-ui\",\"telemetry.sdk.language\":\"webjs\",\"telemetry.sdk.name\":\"opentelemetry\",\"telemetry.sdk.version\":\"1.23.0\",\"user\":\"YWVkZWxlbnlp\"}},{\"traceId\":\"e0cb32bafc56f41920988b5189ae8fd3\",\"name\":\"GET\",\"id\":\"db42c88aed06ad0f\",\"kind\":\"CLIENT\",\"timestamp\":1713860461395000,\"duration\":547000,\"localEndpoint\":{\"serviceName\":\"snomio-ui\"},\"tags\":{\"http.method\":\"GET\",\"http.url\":\"https://uat-snomio.ihtsdotools.org/api/status\",\"http.response_content_length\":\"104\",\"http.status_code\":\"200\",\"http.status_text\":\"\",\"http.host\":\"uat-snomio.ihtsdotools.org\",\"http.scheme\":\"https\",\"http.user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36\",\"service.name\":\"snomio-ui\",\"telemetry.sdk.language\":\"webjs\",\"telemetry.sdk.name\":\"opentelemetry\",\"telemetry.sdk.version\":\"1.23.0\",\"user\":\"YWVkZWxlbnlp\"},\"annotations\":[{\"timestamp\":1713860461395000,\"value\":\"open\"},{\"timestamp\":1713860461395100,\"value\":\"send\"},{\"timestamp\":1713860461395300,\"value\":\"fetchStart\"},{\"timestamp\":1713860461395300,\"value\":\"domainLookupStart\"},{\"timestamp\":1713860461395300,\"value\":\"domainLookupEnd\"},{\"timestamp\":1713860461395300,\"value\":\"connectStart\"},{\"timestamp\":1713860461395300,\"value\":\"secureConnectionStart\"},{\"timestamp\":1713860461395300,\"value\":\"connectEnd\"},{\"timestamp\":1713860461397400,\"value\":\"requestStart\"},{\"timestamp\":1713860461940800,\"value\":\"responseStart\"},{\"timestamp\":1713860461941200,\"value\":\"responseEnd\"},{\"timestamp\":1713860461942000,\"value\":\"loaded\"}]}]";
+  private String OTEL_TELEMETRY_EXAMPLE;
+  private String ZIPKIN_TELEMETRY_EXAMPLE;
+
   @Mock private Authentication authentication;
 
   @BeforeEach
   void setup() throws Exception {
+    OTEL_TELEMETRY_EXAMPLE =
+        new String(
+            Files.readAllBytes(
+                Paths.get(
+                    getClass().getClassLoader().getResource("test-otlp-telemetry.json").toURI())));
+    ZIPKIN_TELEMETRY_EXAMPLE =
+        new String(
+            Files.readAllBytes(
+                Paths.get(
+                    getClass()
+                        .getClassLoader()
+                        .getResource("test-zipkin-telemetry.json")
+                        .toURI())));
     MockitoAnnotations.openMocks(this);
     SecurityContext securityContext = mock(SecurityContext.class);
     Map<String, Object> user = new HashMap<String, Object>();
