bf65b267dcb5cd89f3630bba65c60b60168be9ed
Deanne Ukovich <Deanne.Ukovich@csiro.au>
2024-05-01 10:49:09 +1000

    forgot to include changes to this file to keep track of if the threshold is exceeded

--- eclrefset/src/main/java/com/csiro/eclrefset/LogThresholdInfo.java
+++ eclrefset/src/main/java/com/csiro/eclrefset/LogThresholdInfo.java
@@ -4,16 +4,24 @@ import lombok.extern.java.Log;
 
 @Log
 public final class LogThresholdInfo {
+
+    private static final int THRESHOLD_EXCEEDED_ERROR_CODE = 1; 
     
-    public static void logAdd(Integer addCount, Integer totalCount, double percentChangeThreshold) {
-        log("add", addCount, totalCount, percentChangeThreshold);
+    /** returns 1 if threshold exceeded, otherwise 1 */
+    public static int logAdd(Integer addCount, Integer totalCount, double percentChangeThreshold) {
+        return log("add", addCount, totalCount, percentChangeThreshold);
     }
 
-    public static void logRemove(Integer removeCount, Integer totalCount, double percentChangeThreshold) {
-        log("remove", removeCount, totalCount, percentChangeThreshold);
+    /** returns 1 if threshold exceeded, otherwise 1 */
+    public static int logRemove(Integer removeCount, Integer totalCount, double percentChangeThreshold) {
+        return log("remove", removeCount, totalCount, percentChangeThreshold);
     }
 
-    private static void log(String mode, Integer count, Integer totalCount, double percentChangeThreshold) {
+    /** returns 1 if threshold exceeded, otherwise 1 */
+    private static int log(String mode, Integer count, Integer totalCount, double percentChangeThreshold) {
+
+        int return_code = 0;
+
         log.info("### To " + mode + " count: " + count);
         if (count > 0) {
             double countThreshold = totalCount * percentChangeThreshold;
@@ -24,10 +32,13 @@ public final class LogThresholdInfo {
             else if (Double.compare(count, countThreshold) > 0) {
                 //TODO: notify by email
                 log.info("### WARNING: " + count + " exceeds the " + percentChangeThreshold  + " threshold of " + countThreshold);
+                return_code = THRESHOLD_EXCEEDED_ERROR_CODE;
             }
             else {
                 log.info("### INFO: " + count + " does not exceed the " + percentChangeThreshold  + " threshold of " + countThreshold);
             }
         }
+
+        return return_code;
     }
 }
