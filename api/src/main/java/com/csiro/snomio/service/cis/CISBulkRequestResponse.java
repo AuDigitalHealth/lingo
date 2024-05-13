78423d4e4803e96907a752aa764888da7337c2e3
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2024-05-13 17:28:04 +1000

    Add exception handling and improve operations in CISClient
    This commit introduces CISClientProblem, a new exception for handling CIS client integration problems. CIS errors are now handled and logged inside CISClient, providing more contextual error messages. The CISBulkJobStatusResponse and CISBulkRequestResponse classes have been added or updated with the Lombok @Data annotation for simplicity. The CISClient has seen a major overhaul to improve how operations are executed, including better handling of bulk jobs and timeouts, and fetching generated SCT IDs.

--- api/src/main/java/com/csiro/snomio/service/cis/CISBulkRequestResponse.java
+++ api/src/main/java/com/csiro/snomio/service/cis/CISBulkRequestResponse.java
@@ -1,17 +1,10 @@
 package com.csiro.snomio.service.cis;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CISBulkRequestResponse {
-
   private String id;
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
 }
