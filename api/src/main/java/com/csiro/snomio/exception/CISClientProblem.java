fd31cff09dd7dae2589e0439872b23607764b014
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2024-05-16 12:12:08 +1000

    Refactor error handling in CISClient
    This commit refactors the CISClient to improve error handling. Errors related to operations now use a common static method for consistent, clearer messages. The null check for responses has also been refactored for clarity and to address static analysis warnings.

--- api/src/main/java/com/csiro/snomio/exception/CISClientProblem.java
+++ api/src/main/java/com/csiro/snomio/exception/CISClientProblem.java
@@ -19,4 +19,12 @@ public class CISClientProblem extends SnomioProblem {
         message,
         e);
   }
+
+  public static CISClientProblem cisClientProblemForOperation(String operation, Throwable e) {
+    return new CISClientProblem("Failed to " + operation + " identifiers.", e);
+  }
+
+  public static CISClientProblem cisClientProblemForOperation(String operation) {
+    return new CISClientProblem("Failed to " + operation + " identifiers.");
+  }
 }
