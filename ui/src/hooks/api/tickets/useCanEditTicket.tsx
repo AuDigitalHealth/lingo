07e4e14f8e120d61ec95136979a51339e1caf3ba
Clinton Gillespie <clinton.gillespie@csiro.au>
2024-05-21 20:40:18 +1000

    break out ticketdto search and individual ticket searches into their own hooks

--- ui/src/hooks/api/tickets/useCanEditTicket.tsx
+++ ui/src/hooks/api/tickets/useCanEditTicket.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import useTicketById from '../../useTicketById.tsx';
+import useTicketDtoById from '../../useTicketById.tsx';
 import { Ticket } from '../../../types/tickets/ticket.ts';
 
 export function useCanEditTicketById(ticketId: string | undefined) {
-  const { ticket } = useTicketById(ticketId);
+  const { ticket } = useTicketDtoById(ticketId);
   const [canEdit, setCanEdit] = useState(false);
   useEffect(() => {
     setCanEdit(ticket?.state?.label.toLowerCase().trim() !== 'closed');
