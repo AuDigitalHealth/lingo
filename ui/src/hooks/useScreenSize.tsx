acc8e20f65ebe26b2bf4ac57e9901b4bf2dbdc21
Clinton Gillespie <clintongillespie@outlook.com>
2024-05-27 13:12:38 +1000

    Concept diagram setup (#811)
    * temp
    
    * temp
    
    * change modal to resize when the concept diagram is large or small, setting max height to 80vh/vw
    
    * fix lint error

---
+++ ui/src/hooks/useScreenSize.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+export interface ScreenSize {
+  width: number;
+  height: number;
+}
+const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState<ScreenSize>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return screenSize;
+};
+
+export default useScreenSize;
