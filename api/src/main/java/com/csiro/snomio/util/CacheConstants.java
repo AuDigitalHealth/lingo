7ff3debaff8b664e3c149d61723015bc2a3db454
senjo-aehrc <135300932+senjo-aehrc@users.noreply.github.com>
2024-08-07 10:13:29 +1000

    Create an exclusion list for substances (#1040)
    * Create an exclusion list for substances
    
    * Fix test

--- api/src/main/java/com/csiro/snomio/util/CacheConstants.java
+++ api/src/main/java/com/csiro/snomio/util/CacheConstants.java
@@ -9,6 +9,7 @@ public class CacheConstants {
   public static final String ALL_TASKS_CACHE = "all-tasks";
   public static final String COMPOSITE_UNIT_CACHE = "composite-unit";
   public static final String UNIT_NUMERATOR_DENOMINATOR_CACHE = "unit-numerator-denominator";
+  public static final String VALIDATION_EXCLUDED_SUBSTANCES = "validation-excluded-substances";
 
   private CacheConstants() {
     throw new IllegalStateException("Utility class");
