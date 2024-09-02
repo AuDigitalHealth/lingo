60d05ef158072d9bd335bbe2d231b6faa1d3c170
Clinton Gillespie <clintongillespie@outlook.com>
2024-09-02 14:38:39 +1000

    Snodeine send jobresult to snomio (#1095)
    * send a message to snomio at the end of the job with information on what was added and removed from refsets

--- eclrefset/src/main/java/com/csiro/eclrefset/LogThresholdInfo.java
+++ eclrefset/src/main/java/com/csiro/eclrefset/LogThresholdInfo.java
@@ -1,24 +1,31 @@
 package com.csiro.eclrefset;
 
+import com.csiro.tickets.JobResultDto.ResultDto;
+import com.csiro.tickets.JobResultDto.ResultDto.ResultNotificationDto;
+import com.csiro.tickets.JobResultDto.ResultDto.ResultNotificationDto.ResultNotificationType;
 import lombok.extern.java.Log;
 
 @Log
 public final class LogThresholdInfo {
 
-    private static final int THRESHOLD_EXCEEDED_ERROR_CODE = 1; 
+    private static final int THRESHOLD_EXCEEDED_ERROR_CODE = 1;
     
+    public static final String ACTION_HAS_NOT_BEEN_CARRIED_OUT_MESSAGE = "### This action HAS NOT been carried out.  You will need to investigate and fix the ECL, or override the count threshold check by setting the ignore-refset-count-change-threshold-error variable to true";
+
+    public static final String ACTION_HAS_BEEN_CARRIED_OUT_MESSAGE = "### As you have chosen to IGNORE this warning, this action HAS been carried out.";
+    public static final String COUNT_THRESHOLD_MESSAGE = "ERROR: %d has exceeded the COUNT threshold of %d for refset %s while attempting to %s concepts";
     /** returns 1 if threshold exceeded, otherwise 1 */
-    public static int logAdd(String refSetId, Integer addCount, Integer totalCount, double percentChangeThreshold, FileAppender fileAppender) {
-        return log(refSetId, "add", addCount, totalCount, percentChangeThreshold, fileAppender);
+    public static int logAdd(String refSetId, Integer addCount, Integer totalCount, double percentChangeThreshold, FileAppender fileAppender, ResultDto resultDto) {
+        return log(refSetId, "add", addCount, totalCount, percentChangeThreshold, fileAppender, resultDto);
     }
 
     /** returns 1 if threshold exceeded, otherwise 1 */
-    public static int logRemove(String refSetId, Integer removeCount, Integer totalCount, double percentChangeThreshold, FileAppender fileAppender) {
-        return log(refSetId, "remove", removeCount, totalCount, percentChangeThreshold, fileAppender);
+    public static int logRemove(String refSetId, Integer removeCount, Integer totalCount, double percentChangeThreshold, FileAppender fileAppender, ResultDto resultDto) {
+        return log(refSetId, "remove", removeCount, totalCount, percentChangeThreshold, fileAppender, resultDto);
     }
 
     /** returns 1 if threshold exceeded, otherwise 1 */
-    private static int log(String refSetId, String mode, Integer count, Integer totalCount, double percentChangeThreshold, FileAppender fileAppender) {
+    private static int log(String refSetId, String mode, Integer count, Integer totalCount, double percentChangeThreshold, FileAppender fileAppender, ResultDto resultDto) {
 
         int return_code = 0;
 
@@ -30,8 +37,18 @@ public final class LogThresholdInfo {
                 log.info("### INFO: no pre-existing content for reference set, skipping threshold calculation");
             }
             else if (Double.compare(count, countThreshold) > 0) {
-                log.info("### WARNING: " + count + " exceeds the " + percentChangeThreshold  + " PERCENT threshold of " + countThreshold);
-                log.info("### This action has been carried out, this is just a notification");
+                String warningMessage = "### WARNING: " + count + " exceeds the " + percentChangeThreshold  + " PERCENT threshold of " + countThreshold;
+                log.info(warningMessage);
+                String carriedOutMessage = "### This action has been carried out, this is just a notification";
+                log.info(carriedOutMessage);
+
+                if(resultDto.getNotification() != null){
+                    resultDto.getNotification().setDescription(resultDto.getNotification().getDescription() + ". " + warningMessage + ". " + carriedOutMessage);
+                } else {
+                    resultDto.setNotification(ResultNotificationDto.builder().type(
+                        ResultNotificationType.WARNING).description(warningMessage + ". " + carriedOutMessage).build());
+                }
+
                 // store all the details in a file that will get emailed to notify of the percent threshold been exceeded;
                 fileAppender.appendToFile("### WARNING: Attempting to " + mode + " " + count +  " members for refset " + refSetId + " has exceeded the PERCENT threshold of " + percentChangeThreshold + ".");
                 fileAppender.appendToFile("### This action has been carried out, this is just a notification.");
--- ui/src/types/tickets/jobs.ts
+++ ui/src/types/tickets/jobs.ts
@@ -7,12 +7,15 @@ export interface JobResult extends BaseEntity {
   finishedTime: string;
   results: Result[];
   acknowledged: boolean;
+  nestedError?: string;
 }
 
 export interface Result {
   name: string;
   count: number;
-  items: ResultItem[];
+  results?: Result[];
+  items?: ResultItem[];
+  notification?: ResultNotification;
   id?: number;
 }
 
@@ -20,4 +23,32 @@ export interface ResultItem {
   id: string;
   title?: string;
   link?: string;
+}
+
+export interface ResultNotification {
+  type: ResultNotificationType;
+  description: string;
+}
+
+export enum ResultNotificationType {
+  ERROR = 'ERROR',
+  WARNING = 'WARNING',
+  SUCCESS = 'SUCCESS',
+}
+
+export const getSeverityRank = (type: ResultNotificationType): number => {
+  switch (type) {
+    case ResultNotificationType.ERROR:
+      return 3;
+    case ResultNotificationType.WARNING:
+      return 2;
+    case ResultNotificationType.SUCCESS:
+      return 1;
+    default:
+      return 0;
+  }
+};
+
+export function isJobResult(result: JobResult | Result): result is JobResult {
+  return 'jobName' in result;
 }
