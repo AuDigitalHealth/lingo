e1111f9631686714b235b724ba4b1a7e248d609b
Clinton Gillespie <clintongillespie@outlook.com>
2024-09-02 14:38:39 +1000

    Snodeine send jobresult to snomio (#1095)
    * send a message to snomio at the end of the job with information on what was added and removed from refsets

--- ui/src/hooks/api/useJobResults.tsx
+++ ui/src/hooks/api/useJobResults.tsx
@@ -6,14 +6,19 @@ import {
 } from '@tanstack/react-query';
 import JobResultsService from '../../api/JobResultsService';
 import { useMemo } from 'react';
-import { JobResult } from '../../types/tickets/jobs';
+import {
+  JobResult,
+  Result,
+  ResultNotificationType,
+  getSeverityRank,
+} from '../../types/tickets/jobs';
 
 export const useJobResultsOptions = () => {
   const queryKey = ['job-results'];
   return queryOptions({
     queryKey,
     retry: false,
-    staleTime: 1 * 60 * 1000,
+    refetchInterval: 2 * 60 * 1000,
     queryFn: () => {
       return JobResultsService.getJobResults();
     },
@@ -27,13 +32,61 @@ export function useJobResults() {
 
   const actualData = useMemo(() => {
     let index = 0;
+
+    const createResultWithNestedNotifications = (result: Result): Result => {
+      const temp = { ...result };
+      let highestSeverity: ResultNotificationType | null = null;
+      let combinedDescription = '';
+
+      temp.results?.forEach(result => {
+        if (result.notification) {
+          // Combine descriptions
+          combinedDescription +=
+            (combinedDescription ? '\n' : '') + result.notification.description;
+
+          // Determine highest severity
+          if (
+            !highestSeverity ||
+            getSeverityRank(result.notification.type) >
+              getSeverityRank(highestSeverity)
+          ) {
+            highestSeverity = result.notification.type;
+          }
+        }
+      });
+
+      if (highestSeverity) {
+        temp.notification = {
+          type: highestSeverity,
+          description: combinedDescription,
+        };
+      }
+
+      return temp;
+    };
+
+    // Recursive function to handle nested results
+    const assignIdsAndRaiseErrors = (results: Result[]): Result[] => {
+      return results.map(result => {
+        const newResult = {
+          ...result,
+          id: index++,
+        };
+
+        if (newResult.results && Array.isArray(newResult.results)) {
+          newResult.results = assignIdsAndRaiseErrors(newResult.results);
+        }
+
+        return createResultWithNestedNotifications(newResult);
+      });
+    };
+
     const returnVal = data?.map(item => ({
       ...item,
-      results: item.results.map(result => ({
-        ...result,
-        id: index++,
-      })),
+      results: assignIdsAndRaiseErrors(item.results),
     }));
+
+    console.log(returnVal);
     return returnVal;
   }, [data]);
 
