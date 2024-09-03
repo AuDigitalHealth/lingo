a73fda98fcabc3748746276465549cb333c97833
Clinton Gillespie <clintongillespie@outlook.com>
2024-09-03 10:48:11 +1000

    some minor changes to the jobsresults list, namely removed all the ###'s from snodeine, and also added the option to have notification messages in paragraphs, this is done with \n\n (#1104)

--- eclrefset/src/main/java/com/csiro/eclrefset/LogThresholdInfo.java
+++ eclrefset/src/main/java/com/csiro/eclrefset/LogThresholdInfo.java
@@ -10,9 +10,9 @@ public final class LogThresholdInfo {
 
     private static final int THRESHOLD_EXCEEDED_ERROR_CODE = 1;
     
-    public static final String ACTION_HAS_NOT_BEEN_CARRIED_OUT_MESSAGE = "### This action HAS NOT been carried out.  You will need to investigate and fix the ECL, or override the count threshold check by setting the ignore-refset-count-change-threshold-error variable to true";
+    public static final String ACTION_HAS_NOT_BEEN_CARRIED_OUT_MESSAGE = "This action HAS NOT been carried out.  You will need to investigate and fix the ECL, or override the count threshold check by setting the ignore-refset-count-change-threshold-error variable to true";
 
-    public static final String ACTION_HAS_BEEN_CARRIED_OUT_MESSAGE = "### As you have chosen to IGNORE this warning, this action HAS been carried out.";
+    public static final String ACTION_HAS_BEEN_CARRIED_OUT_MESSAGE = "As you have chosen to IGNORE this warning, this action HAS been carried out.";
     public static final String COUNT_THRESHOLD_MESSAGE = "ERROR: %d has exceeded the COUNT threshold of %d for refset %s while attempting to %s concepts";
     /** returns 1 if threshold exceeded, otherwise 1 */
     public static int logAdd(String refSetId, Integer addCount, Integer totalCount, double percentChangeThreshold, FileAppender fileAppender, ResultDto resultDto) {
@@ -37,16 +37,16 @@ public final class LogThresholdInfo {
                 log.info("### INFO: no pre-existing content for reference set, skipping threshold calculation");
             }
             else if (Double.compare(count, countThreshold) > 0) {
-                String warningMessage = "### WARNING: " + count + " exceeds the " + percentChangeThreshold  + " PERCENT threshold of " + countThreshold;
-                log.info(warningMessage);
-                String carriedOutMessage = "### This action has been carried out, this is just a notification";
-                log.info(carriedOutMessage);
+                String warningMessage = "WARNING: " + count + " exceeds the " + percentChangeThreshold  + " PERCENT threshold of " + countThreshold;
+                log.info("###" + warningMessage);
+                String carriedOutMessage = "This action has been carried out, this is just a notification";
+                log.info("###" + carriedOutMessage);
 
                 if(resultDto.getNotification() != null){
                     resultDto.getNotification().setDescription(resultDto.getNotification().getDescription() + ". " + warningMessage + ". " + carriedOutMessage);
                 } else {
                     resultDto.setNotification(ResultNotificationDto.builder().type(
-                        ResultNotificationType.WARNING).description(warningMessage + ". " + carriedOutMessage).build());
+                        ResultNotificationType.WARNING).description(warningMessage + "\n\n " + carriedOutMessage).build());
                 }
 
                 // store all the details in a file that will get emailed to notify of the percent threshold been exceeded;
