c80be65b6d9c0e8db8e978efb445182cb487c1a5
leoniedickson <140992055+leoniedickson@users.noreply.github.com>
2024-09-18 12:36:05 +1000

    Snodine - tick and flick refsets (#1121)
    * rename routes and labels to snodine
    
    * point ecl builder at tx
    
    * list tick and flick refsets in tab
    
    * use constant
    
    * add route for tnf details, update routes for query spec pages
    
    * separate component for refset concept details
    
    * display tnf refset details
    
    * use concept label for breadcrumb
    
    * rename components
    
    * display list of refset members
    
    * filter members by term and toggle inactive status
    
    * pass params to refset members call, rename hook
    
    * separate confirm modal component
    
    * retire member from list
    
    * add refset members, disable retire button if user is not task owner
    
    * remove active and primitive fields from table
    
    * make inactive concepts pop more
    
    * use mui utils capitalize function
    
    * checkboxes to retire multiple members
    
    * toggle to include qs refsets and indicate if refset is qs in table
    
    * check refsets for inactive concepts
    
    * indicate if refset is query-based/has inactives in details page
    
    * toggle for AU refsets only
    
    * remove selected concepts from search results
    
    * validate concept ids before adding as refset members
    
    * batch refset member searches, update hook names
    
    * show table in loading state when query refsets are loading
    
    * run prettier
    
    * add snowstorm proxy for snodine concept requests
    
    * add CVE-2024-38816 to .trivyignore
    
    * disable closing of dialogs when actions are pending
    
    * add termActive filter for add members concept search
    
    * dialog button changes
    
    * add termActive filter for concept search in members list
    
    * contained cancel buttons
    
    * replace members in a tick and flick refset

---
+++ ui/src/pages/eclRefset/Refsets.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import useUserTaskByIds from '../../hooks/eclRefset/useUserTaskByIds.tsx';
+import { useNavigate, useParams } from 'react-router-dom';
+import QueryRefsetList from './components/QueryRefsetList.tsx';
+import { Box, Button, Stack, Tab } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { PlusCircleOutlined } from '@ant-design/icons';
+import useUserStore from '../../stores/UserStore.ts';
+import TickFlickRefsetList from './components/TickFlickRefsetList.tsx';
+
+type RefsetType = 'query' | 'tickflick';
+
+function Refsets() {
+  const navigate = useNavigate();
+  const { taskKey, projectKey } = useParams();
+  const task = useUserTaskByIds();
+  const { login } = useUserStore();
+
+  const branch =
+    task?.branchPath ?? `MAIN/SNOMEDCT-AU/${projectKey}/${taskKey}`;
+
+  const [tab, setTab] = useState<RefsetType>('query');
+
+  return (
+    <Box
+      sx={{
+        '& .MuiTabPanel-root': {
+          padding: 0,
+        },
+      }}
+    >
+      <TabContext value={tab}>
+        <TabList onChange={(_, value) => setTab(value as RefsetType)}>
+          <Tab label="Query-Based" value="query" />
+          <Tab label="Tick and Flick" value="tickflick" />
+        </TabList>
+        <TabPanel value="query">
+          <Box>
+            <Stack sx={{ width: '100%', padding: '0em 0em 1em 1em' }}>
+              <Button
+                variant="contained"
+                color="success"
+                startIcon={<PlusCircleOutlined />}
+                sx={{ marginLeft: 'auto' }}
+                disabled={login !== task?.assignee.username}
+                onClick={() => navigate('qs')}
+              >
+                Create
+              </Button>
+            </Stack>
+
+            <QueryRefsetList branch={branch} />
+          </Box>
+        </TabPanel>
+        <TabPanel value="tickflick">
+          <TickFlickRefsetList branch={branch} />
+        </TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
+
+export default Refsets;
