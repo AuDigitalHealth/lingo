ffcd34274653014bcd0cc325f7bc79c86b62e747
Clinton Gillespie <clintongillespie@outlook.com>
2024-09-29 19:57:55 +1000

    Bugfix/websocket backoff (#1184)
    * fix websocket spam that can occur when you are logged into snomio, and the server dies whilst you are already connected to the websocket. The connection attemp now backs off to the max of 2*n where n is attempts * 1s, or 45s

--- ui/src/hooks/useWebSocket.tsx
+++ ui/src/hooks/useWebSocket.tsx
@@ -1,7 +1,7 @@
 import SockJs from 'sockjs-client';
-import Stomp, { Frame, Message } from 'stompjs';
+import Stomp from 'stompjs';
 import useUserStore from '../stores/UserStore';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import useWebsocketEventHandler from './useWebsocketEventHandler';
 
 export enum EntityType {
@@ -16,66 +16,114 @@ export enum EntityType {
   AuthorChange = 'AuthorChange',
 }
 
-// using a hook, so we can create snackbars on different events
+const maxReconnectDelay = 45 * 1000;
+
 function useWebSocket() {
   const { handleClassificationEvent, handleValidationEvent } =
     useWebsocketEventHandler();
-
-  let stompClient: Stomp.Client;
+  const stompClientRef = useRef<Stomp.Client | null>(null);
   const user = useUserStore();
+  const reconnectAttemptsRef = useRef(0);
+  const initialReconnectDelay = 1000;
 
-  const stompConnectCallback = (frame: Frame | undefined): any => {
-    const headers = frame?.headers as StompHeaders;
-    const username = headers['user-name'];
-    if (username !== null) {
-      stompClient.subscribe(
-        `/topic/user/${user?.login}/notifications`,
-        subscriptionHandler,
-        { id: `sca-subscription-id-${user?.login}` },
-      );
+  const stompConnectCallback = useCallback(
+    (frame: Stomp.Frame | undefined): void => {
+      const headers = frame?.headers as StompHeaders;
+      const username = headers['user-name'];
+      if (username !== null && stompClientRef.current) {
+        stompClientRef.current.subscribe(
+          `/topic/user/${user?.login}/notifications`,
+          subscriptionHandler,
+          { id: `sca-subscription-id-${user?.login}` },
+        );
+      }
+      reconnectAttemptsRef.current = 0;
+    },
+    // eslint-disable-next-line
+    [user?.login],
+  );
+
+  const subscriptionHandler = useCallback(
+    (message: Stomp.Message) => {
+      const notification = JSON.parse(message.body) as StompMessage;
+      switch (notification.entityType) {
+        case EntityType.Classification:
+          void handleClassificationEvent(notification);
+          break;
+        case EntityType.Validation:
+        case EntityType.Promotion:
+          void handleValidationEvent(notification);
+          break;
+        default:
+          break;
+      }
+    },
+    [handleClassificationEvent, handleValidationEvent],
+  );
+
+  const errorCallback = useCallback((error: Stomp.Frame | string): void => {
+    console.log(error);
+    safeDeactivate();
+    reconnectWithBackoff();
+    // eslint-disable-next-line
+  }, []);
+
+  const reconnectWithBackoff = useCallback(() => {
+    let delay = 1000;
+    if (reconnectAttemptsRef.current > 5) {
+      delay = maxReconnectDelay;
+    } else {
+      delay = initialReconnectDelay * Math.pow(2, reconnectAttemptsRef.current);
     }
-  };
+    setTimeout(() => {
+      reconnectAttemptsRef.current++;
+      stompConnect();
+    }, delay);
+    // eslint-disable-next-line
+  }, []);
 
-  const stompConnect = () => {
+  const safeDeactivate = useCallback(() => {
+    if (stompClientRef.current) {
+      if (stompClientRef.current.connected) {
+        stompClientRef.current.disconnect(() => {
+          return undefined;
+        });
+      } else {
+        // If not connected, just remove the reference
+        stompClientRef.current = null;
+      }
+    }
+  }, []);
+
+  const stompConnect = useCallback(() => {
+    // Check if already connected or connecting
+    if (stompClientRef.current && stompClientRef.current.connected) {
+      return;
+    }
+    safeDeactivate();
+
     const sockJsProtocols = ['websocket'];
     const socketProvider = new SockJs(
-      '/authoring-services/' + 'authoring-services-websocket',
+      '/authoring-services/authoring-services-websocket',
       null,
       { transports: sockJsProtocols },
     );
 
-    const stompyBoi: Stomp.Client = Stomp.over(socketProvider);
+    const stompyBoi = Stomp.over(socketProvider);
     stompyBoi.connect({}, stompConnectCallback, errorCallback);
 
-    stompClient = stompyBoi;
-  };
-  const subscriptionHandler = (message: Message) => {
-    const notification = JSON.parse(message.body) as StompMessage;
-    switch (notification.entityType) {
-      case EntityType.Classification:
-        void handleClassificationEvent(notification);
-        break;
-      case EntityType.Validation:
-        void handleValidationEvent(notification);
-        break;
-      case EntityType.Promotion:
-        void handleValidationEvent(notification);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const errorCallback = (frame: string | Frame): any => {
-    stompClient.disconnect(stompConnect);
-    setTimeout(function () {
-      stompConnect();
-    }, 5000);
-  };
+    stompClientRef.current = stompyBoi;
+    // eslint-disable-next-line
+  }, [stompConnectCallback, errorCallback]);
 
   useEffect(() => {
     stompConnect();
-  }, []);
+    return () => {
+      safeDeactivate();
+    };
+  }, [stompConnect, safeDeactivate]);
+
+  return { stompClient: stompClientRef.current };
 }
 
 interface StompHeaders {
