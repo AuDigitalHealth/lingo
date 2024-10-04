f05b54c5c302dc2a015f8c6d990ba4c6c7cabcb0
Clinton Gillespie <clintongillespie@outlook.com>
2024-10-04 17:01:26 +1000

    Bugfix/filter issues (#1202)
    * uiSearchConfiguration is no longer cached, fixed some issues with loaded filters.
    
    * fix bug where if a filter was updated, that is on the users backlog tables page, it would not be reflected there until the page is reloaded, update dependency for commons-io in sergio-extension/pom.xml

--- ui/src/pages/tickets/UserDefinedTables.tsx
+++ ui/src/pages/tickets/UserDefinedTables.tsx
@@ -41,7 +41,7 @@ import { Route, Routes } from 'react-router-dom';
 import TicketDrawer from './components/grid/TicketDrawer';
 
 export default function UserDefinedTables() {
-  const { isLoading, data } = useUiSearchConfiguration();
+  const { isFetching, data } = useUiSearchConfiguration();
   const [editable, setEditable] = useState(false);
   const [addModalOpen, setAddModalOpen] = useState(false);
   const [quadrantToAdd, setQuadrantToAdd] = useState<number | undefined>();
@@ -63,10 +63,6 @@ export default function UserDefinedTables() {
     () => dndItems?.map(item => item.id),
     [dndItems],
   ) as (UniqueIdentifier | { id: UniqueIdentifier })[];
-
-  useEffect(() => {
-    console.log(dndItems);
-  }, [dndItems]);
 
   const pointerSensor = useSensor(PointerSensor, {
     activationConstraint: {
@@ -168,16 +164,16 @@ export default function UserDefinedTables() {
     [setAddModalOpen, setQuadrantToAdd],
   );
 
-  const findByGrouping = (
-    configurations: UiSearchConfiguration[] | undefined,
-    grouping: number,
-  ) => {
-    return configurations
-      ? configurations.find(item => {
-          return item.grouping === grouping;
-        })
-      : undefined;
-  };
+  const findByGrouping = useCallback(
+    (configurations: UiSearchConfiguration[] | undefined, grouping: number) => {
+      return configurations
+        ? configurations.find(item => {
+            return item.grouping === grouping;
+          })
+        : undefined;
+    },
+    [],
+  );
 
   useEffect(() => {
     const createItems = () => {
@@ -197,9 +193,43 @@ export default function UserDefinedTables() {
     };
     setDndItems(createItems());
   }, [data]);
+
+  const tables = useMemo(() => {
+    return (
+      <>
+        {isFetching ? (
+          <Loading />
+        ) : (
+          <>
+            <UserDefinedTableQuadrant
+              allConfigurations={data}
+              uiSearchConfiguration={findByGrouping(data, 0)}
+              count={data?.length}
+            />
+            <UserDefinedTableQuadrant
+              allConfigurations={data}
+              uiSearchConfiguration={findByGrouping(data, 1)}
+              count={data?.length}
+            />
+            <UserDefinedTableQuadrant
+              allConfigurations={data}
+              uiSearchConfiguration={findByGrouping(data, 2)}
+              count={data?.length}
+            />
+            <UserDefinedTableQuadrant
+              allConfigurations={data}
+              uiSearchConfiguration={findByGrouping(data, 3)}
+              count={data?.length}
+            />
+          </>
+        )}
+      </>
+    );
+  }, [data, findByGrouping, isFetching]);
+
   return (
     <>
-      {isLoading ? (
+      {isFetching ? (
         <Loading />
       ) : (
         <>
@@ -254,28 +284,7 @@ export default function UserDefinedTables() {
               </DndContext>
             ) : (
               <>
-                <>
-                  <UserDefinedTableQuadrant
-                    allConfigurations={data}
-                    uiSearchConfiguration={findByGrouping(data, 0)}
-                    count={data?.length}
-                  />
-                  <UserDefinedTableQuadrant
-                    allConfigurations={data}
-                    uiSearchConfiguration={findByGrouping(data, 1)}
-                    count={data?.length}
-                  />
-                  <UserDefinedTableQuadrant
-                    allConfigurations={data}
-                    uiSearchConfiguration={findByGrouping(data, 2)}
-                    count={data?.length}
-                  />
-                  <UserDefinedTableQuadrant
-                    allConfigurations={data}
-                    uiSearchConfiguration={findByGrouping(data, 3)}
-                    count={data?.length}
-                  />
-                </>
+                <>{tables}</>
               </>
             )}
             <Routes>
