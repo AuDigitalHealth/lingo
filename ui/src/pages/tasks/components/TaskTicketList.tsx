1b53ab8a9e378764c076e73b8551e22ea1ed4d51
Clinton Gillespie <clintongillespie@outlook.com>
2024-10-25 14:12:42 +1000

    fix unselectedable text in /jobResults and the backlog (#1247)
    * fix unselectedable text in /jobResults and the backlog
    
    * fix in task ticket list

--- ui/src/pages/tasks/components/TaskTicketList.tsx
+++ ui/src/pages/tasks/components/TaskTicketList.tsx
@@ -3,7 +3,6 @@ import {
   IconButton,
   List,
   ListItem,
-  ListItemButton,
   ListItemIcon,
   ListItemText,
   Typography,
@@ -164,41 +163,47 @@ function TaskTicketPage({
         key={useTicketQuery.data.id}
         sx={{ width: '100%', display: 'flex', flexDirection: 'row' }}
       >
-        <Link
-          to={`${useTicketQuery.data.ticketNumber}`}
-          key={useTicketQuery.data.ticketNumber}
-          style={{ textDecoration: 'none', color: 'inherit', width: '100%' }}
-        >
-          <ListItemButton>
+        <ListItem>
+          <Link
+            to={`${useTicketQuery.data.ticketNumber}`}
+            key={useTicketQuery.data.ticketNumber}
+            style={{ textDecoration: 'none' }}
+          >
             <ListItemIcon sx={{ minWidth: '56px' }}>
-              <Folder sx={{ color: `${theme.palette.grey[600]}` }} />
+              <IconButton>
+                <Folder sx={{ color: `${theme.palette.grey[600]}` }} />
+              </IconButton>
             </ListItemIcon>
+          </Link>
+          {/* Ticket number and title stacked */}
+          <ListItemText
+            primary={
+              <>
+                {/* Ticket Number */}
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ userSelect: 'text' }}
+                >
+                  {useTicketQuery.data.ticketNumber}
+                </Typography>
+                {/* Title */}
+                <Typography variant="body1">
+                  {useTicketQuery.data.title}
+                </Typography>
+                {/*Highlight tickets under review that have no products (status: completed) and no bulk products.*/}
+                {useTicketQuery.data.state?.label === 'Review' &&
+                  useTicketQuery.data.products?.every(p => !p.conceptId) &&
+                  useTicketQuery.data.bulkProductActions?.length === 0 && (
+                    <Typography variant="body2" color="orange">
+                      Missing saved product data
+                    </Typography>
+                  )}
+              </>
+            }
+          />
+        </ListItem>
 
-            {/* Ticket number and title stacked */}
-            <ListItemText
-              primary={
-                <>
-                  {/* Ticket Number */}
-                  <Typography variant="body2" color="textSecondary">
-                    {useTicketQuery.data.ticketNumber}
-                  </Typography>
-                  {/* Title */}
-                  <Typography variant="body1">
-                    {useTicketQuery.data.title}
-                  </Typography>
-                  {/*Highlight tickets under review that have no products (status: completed) and no bulk products.*/}
-                  {useTicketQuery.data.state?.label === 'Review' &&
-                    useTicketQuery.data.products?.every(p => !p.conceptId) &&
-                    useTicketQuery.data.bulkProductActions?.length === 0 && (
-                      <Typography variant="body2" color="orange">
-                        Missing saved product data
-                      </Typography>
-                    )}
-                </>
-              }
-            />
-          </ListItemButton>
-        </Link>
         <UnableToEditTooltip
           canEdit={canEdit}
           lockDescription={lockDescription}
