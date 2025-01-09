a1797af3dc9f8d556328e5e1544aa0ce5043acb9
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2025-01-09 10:41:59 +1000

    Latest commit to switch to def

--- ui/src/pages/products/rjsf/AutoCompleteField.tsx
+++ ui/src/pages/products/rjsf/AutoCompleteField.tsx
@@ -1,91 +1,116 @@
-import React, { useState, useEffect } from "react";
-import { FieldProps } from "@rjsf/core";
-import { Autocomplete, CircularProgress, TextField, Typography } from "@mui/material";
-import { useSearchConceptsByEcl } from "../../../hooks/api/useInitializeConcepts";
-import { Concept } from "../../../types/concept";
+import React, { useState, useEffect } from 'react';
+import { FieldProps } from '@rjsf/core';
+import {
+  Autocomplete,
+  CircularProgress,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useSearchConceptsByEcl } from '../../../hooks/api/useInitializeConcepts';
+import { Concept } from '../../../types/concept';
 
 const AutoCompleteField = ({
-                               schema,
-                               uiSchema,
-                               formData,
-                               onChange,
+                             schema,
+                             uiSchema,
+                             formData,
+                             onChange,
                            }: FieldProps) => {
-    const { branch, ecl, showDefaultOptions } = uiSchema["ui:options"] || {};
-    const [inputValue, setInputValue] = useState(""); // For input field text
-    const [options, setOptions] = useState<Concept[]>([]); // Options from the search API
-    const { isLoading, allData } = useSearchConceptsByEcl(inputValue, ecl, branch, showDefaultOptions);
+  const { branch, ecl, showDefaultOptions } = uiSchema['ui:options'] || {};
+  const [inputValue, setInputValue] = useState(''); // For input field text
+  const [options, setOptions] = useState<Concept[]>([]); // Options from the search API
+  const { isLoading, allData } = useSearchConceptsByEcl(
+      inputValue,
+      ecl,
+      branch,
+      showDefaultOptions,
+  );
 
-    // Title from schema or uiSchema
-    const title = schema.title || uiSchema["ui:title"];
+  // Title from schema or uiSchema
+  const title = schema.title || uiSchema['ui:title'];
 
-    // Update options when search results change
-    useEffect(() => {
-        if (allData) {
-            const uniqueOptions = Array.from(
-                new Map(allData.map((item) => [item.conceptId, item])).values()
-            );
-            setOptions(uniqueOptions);
-        }
-    }, [allData]);
+  // Get the disabled option from uiSchema (defaults to false if not specified)
+  const isDisabled = uiSchema['ui:options']?.disabled || false;
 
-    // Sync inputValue when formData changes
-    useEffect(() => {
-        if (formData) {
-            const selectedOption = options.find((option) => option.conceptId === formData.conceptId);
-            setInputValue(selectedOption?.pt.term || ""); // Set term from selected option
-        }
-    }, [formData, options]);
+  // Update options when search results change
+  useEffect(() => {
+    if (allData) {
+      const uniqueOptions = Array.from(
+          new Map(allData.map(item => [item.conceptId, item])).values(),
+      );
+      setOptions(uniqueOptions);
+    }
+  }, [allData]);
 
-    // Handle option selection
-    const handleProductChange = (selectedProduct: Concept | null) => {
-        if (selectedProduct) {
-            onChange(selectedProduct); // Update formData
-            setInputValue(selectedProduct.pt.term); // Set input text
-        } else {
-            onChange(null); // Clear formData
-            setInputValue(""); // Clear input field
-        }
-    };
+  // Sync inputValue when formData changes
+  useEffect(() => {
+    if (formData) {
+      const selectedOption = options.find(
+          option => option.conceptId === formData.conceptId,
+      );
+      setInputValue(selectedOption?.pt.term || ''); // Set term from selected option
+    }
+  }, [formData, options]);
 
-    return (
-        <div>
-            {/* Render title */}
-            {title && <Typography variant="h6" gutterBottom>{title}</Typography>}
+  // Handle option selection
+  const handleProductChange = (selectedProduct: Concept | null) => {
+    if (selectedProduct) {
+      onChange(selectedProduct); // Update formData
+      setInputValue(selectedProduct.pt.term); // Set input text
+    } else {
+      onChange(null); // Clear formData
+      setInputValue(''); // Clear input field
+    }
+  };
 
-            {/* Autocomplete field */}
-            <Autocomplete
-                loading={isLoading}
-                options={options}
-                getOptionLabel={(option) => option?.pt?.term || ""}
-                value={options.find((option) => option.conceptId === formData?.conceptId) || null}
-                onInputChange={(event, newInputValue) => setInputValue(newInputValue)}
-                onChange={(event, selectedValue) => handleProductChange(selectedValue as Concept)}
-                isOptionEqualToValue={(option, selectedValue) =>
-                    option?.conceptId === selectedValue?.conceptId
-                }
-                renderOption={(props, option) => (
-                    <li {...props} key={option.conceptId}>
-                        {option.pt.term}
-                    </li>
-                )}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        label={schema.title}
-                        InputProps={{
-                            ...params.InputProps,
-                            endAdornment: (
-                                <>
-                                    {isLoading ? <CircularProgress size={20} /> : null}
-                                    {params.InputProps.endAdornment}
-                                </>
-                            ),
-                        }}
-                    />
-                )}
-            />
-        </div>
-    );
+  return (
+      <div>
+        {/* Render title */}
+        {title && (
+            <Typography variant="h6" gutterBottom>
+              {title}
+            </Typography>
+        )}
+
+        {/* Autocomplete field */}
+        <Autocomplete
+            loading={isLoading}
+            options={options}
+            getOptionLabel={option => option?.pt?.term || ''}
+            value={
+                options.find(option => option.conceptId === formData?.conceptId) ||
+                null
+            }
+            onInputChange={(event, newInputValue) => setInputValue(newInputValue)}
+            onChange={(event, selectedValue) =>
+                handleProductChange(selectedValue as Concept)
+            }
+            isOptionEqualToValue={(option, selectedValue) =>
+                option?.conceptId === selectedValue?.conceptId
+            }
+            renderOption={(props, option) => (
+                <li {...props} key={option.conceptId}>
+                  {option.pt.term}
+                </li>
+            )}
+            renderInput={params => (
+                <TextField
+                    {...params}
+                    label={title}
+                    InputProps={{
+                      ...params.InputProps,
+                      endAdornment: (
+                          <>
+                            {isLoading ? <CircularProgress size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                          </>
+                      ),
+                    }}
+                />
+            )}
+            disabled={isDisabled} // Disable the autocomplete based on the `disabled` flag
+        />
+      </div>
+  );
 };
 
 export default AutoCompleteField;
--- ui/src/pages/products/rjsf/UnitValueField.tsx
+++ ui/src/pages/products/rjsf/UnitValueField.tsx
@@ -1,61 +1,65 @@
 import React from 'react';
 import { Grid, TextField, Typography } from '@mui/material';
-import AutoCompleteField from "./AutoCompleteField"; // Ensure this path is correct
+import AutoCompleteField from './AutoCompleteField'; // Ensure this path is correct
 import { FieldProps } from '@rjsf/core';
 
-const UnitValueField = ({ formData, onChange, schema, uiSchema }: FieldProps) => {
-    const { value, unit } = formData || { value: '', unit: '' };
+const UnitValueField = ({
+  formData,
+  onChange,
+  schema,
+  uiSchema,
+}: FieldProps) => {
+  const { value, unit } = formData || { value: '', unit: '' };
 
-    const handleUnitChange = (selectedUnit: string | null) => {
-        // Update unit value using the AutoCompleteField's onChange handler
-        onChange({ ...formData, unit: selectedUnit || "" }); // Update formData with the unit conceptId
-    };
+  const handleUnitChange = (selectedUnit: string | null) => {
+    // Update unit value using the AutoCompleteField's onChange handler
+    onChange({ ...formData, unit: selectedUnit || '' }); // Update formData with the unit conceptId
+  };
 
-    // Get title from uiSchema (with fallback if title is not provided)
-    const title = uiSchema?.["ui:title"] || "Unit and Value";
+  // Get title from uiSchema (with fallback if title is not provided)
+  const title = uiSchema?.['ui:title'] || 'Unit and Value';
 
-    // Get only the 'unit' specific options from the uiSchema
-    const unitOptions = uiSchema?.unit?.["ui:options"] || {};
+  // Get only the 'unit' specific options from the uiSchema
+  const unitOptions = uiSchema?.unit?.['ui:options'] || {};
 
-    return (
-        <Grid container spacing={1}>
-            {/* Title from uiSchema */}
-            <Grid item xs={12}>
-                <Typography variant="h6" gutterBottom>
-                    {title}
-                </Typography>
-            </Grid>
+  return (
+    <Grid container spacing={1}>
+      {/* Title from uiSchema */}
+      <Grid item xs={12}>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+      </Grid>
 
-            {/* Value field (e.g., number input) */}
-            <Grid item xs={6}>
-                <TextField
-                    label="Value"
-                    value={value || ''}
-                    onChange={(event) => {
-                        const numericValue = parseFloat(event.target.value);
-                        if (!isNaN(numericValue)) {
-                            onChange({ ...formData, value: numericValue });
-                        }
-                    }}
-                    type="number"
-                    fullWidth={true}
-                />
+      {/* Value field (e.g., number input) */}
+      <Grid item xs={6}>
+        <TextField
+          label="Value"
+          value={value || ''}
+          onChange={event => {
+            const numericValue = parseFloat(event.target.value);
+            if (!isNaN(numericValue)) {
+              onChange({ ...formData, value: numericValue });
+            }
+          }}
+          type="number"
+          fullWidth={true}
+        />
+      </Grid>
 
-            </Grid>
-
-            {/* Unit field with AutoCompleteField */}
-            <Grid item xs={6}>
-                <AutoCompleteField
-                    schema={schema} // Pass schema for AutoCompleteField
-                    formData={unit || ''} // Pass the current unit value
-                    onChange={handleUnitChange} // Handle changes for unit
-                    uiSchema={{
-                        "ui:options": unitOptions, // Pass only the relevant 'unit' options
-                    }}
-                />
-            </Grid>
-        </Grid>
-    );
+      {/* Unit field with AutoCompleteField */}
+      <Grid item xs={6}>
+        <AutoCompleteField
+          schema={schema} // Pass schema for AutoCompleteField
+          formData={unit || ''} // Pass the current unit value
+          onChange={handleUnitChange} // Handle changes for unit
+          uiSchema={{
+            'ui:options': unitOptions, // Pass only the relevant 'unit' options
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default UnitValueField;
