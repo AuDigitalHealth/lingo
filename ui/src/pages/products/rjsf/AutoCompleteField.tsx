464b3fbca16928697e850a98f4933ba801c1bdc5
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2025-01-09 13:36:31 +1000

    Loading existing data

--- ui/src/pages/products/rjsf/AutoCompleteField.tsx
+++ ui/src/pages/products/rjsf/AutoCompleteField.tsx
@@ -10,44 +10,44 @@ import { useSearchConceptsByEcl } from '../../../hooks/api/useInitializeConcepts
 import { Concept } from '../../../types/concept';
 
 const AutoCompleteField = ({
-                             schema,
-                             uiSchema,
-                             formData,
-                             onChange,
-                           }: FieldProps) => {
+  schema,
+  uiSchema,
+  formData,
+  onChange,
+}: FieldProps) => {
   const { branch, ecl, showDefaultOptions } = uiSchema['ui:options'] || {};
-  const [inputValue, setInputValue] = useState(''); // For input field text
-  const [options, setOptions] = useState<Concept[]>([]); // Options from the search API
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState<Concept[]>([]);
   const { isLoading, allData } = useSearchConceptsByEcl(
-      inputValue,
-      ecl,
-      branch,
-      showDefaultOptions,
+    inputValue,
+    ecl,
+    branch,
+    showDefaultOptions,
   );
 
-  // Title from schema or uiSchema
   const title = schema.title || uiSchema['ui:title'];
-
-  // Get the disabled option from uiSchema (defaults to false if not specified)
   const isDisabled = uiSchema['ui:options']?.disabled || false;
 
   // Update options when search results change
   useEffect(() => {
     if (allData) {
       const uniqueOptions = Array.from(
-          new Map(allData.map(item => [item.conceptId, item])).values(),
+        new Map(allData.map(item => [item.conceptId, item])).values(),
       );
       setOptions(uniqueOptions);
     }
   }, [allData]);
 
-  // Sync inputValue when formData changes
+  // Sync inputValue and selected option when formData or options change
   useEffect(() => {
-    if (formData) {
+    if (formData && options.length > 0) {
       const selectedOption = options.find(
-          option => option.conceptId === formData.conceptId,
+        option => option.conceptId === formData.conceptId,
       );
-      setInputValue(selectedOption?.pt.term || ''); // Set term from selected option
+      setInputValue(selectedOption?.pt.term || formData?.pt?.term || '');
+    } else if (formData) {
+      // If options are not yet loaded, fallback to formData value
+      setInputValue(formData?.pt?.term || '');
     }
   }, [formData, options]);
 
@@ -63,53 +63,52 @@ const AutoCompleteField = ({
   };
 
   return (
-      <div>
-        {/* Render title */}
-        {title && (
-            <Typography variant="h6" gutterBottom>
-              {title}
-            </Typography>
-        )}
+    <div>
+      {title && (
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+      )}
 
-        {/* Autocomplete field */}
-        <Autocomplete
-            loading={isLoading}
-            options={options}
-            getOptionLabel={option => option?.pt?.term || ''}
-            value={
-                options.find(option => option.conceptId === formData?.conceptId) ||
-                null
-            }
-            onInputChange={(event, newInputValue) => setInputValue(newInputValue)}
-            onChange={(event, selectedValue) =>
-                handleProductChange(selectedValue as Concept)
-            }
-            isOptionEqualToValue={(option, selectedValue) =>
-                option?.conceptId === selectedValue?.conceptId
-            }
-            renderOption={(props, option) => (
-                <li {...props} key={option.conceptId}>
-                  {option.pt.term}
-                </li>
-            )}
-            renderInput={params => (
-                <TextField
-                    {...params}
-                    label={title}
-                    InputProps={{
-                      ...params.InputProps,
-                      endAdornment: (
-                          <>
-                            {isLoading ? <CircularProgress size={20} /> : null}
-                            {params.InputProps.endAdornment}
-                          </>
-                      ),
-                    }}
-                />
-            )}
-            disabled={isDisabled} // Disable the autocomplete based on the `disabled` flag
-        />
-      </div>
+      <Autocomplete
+        loading={isLoading}
+        options={options}
+        getOptionLabel={option => option?.pt?.term || ''}
+        value={
+          options.find(option => option.conceptId === formData?.conceptId) ||
+          formData ||
+          null
+        }
+        onInputChange={(event, newInputValue) => setInputValue(newInputValue)}
+        onChange={(event, selectedValue) =>
+          handleProductChange(selectedValue as Concept)
+        }
+        isOptionEqualToValue={(option, selectedValue) =>
+          option?.conceptId === selectedValue?.conceptId
+        }
+        renderOption={(props, option) => (
+          <li {...props} key={option.conceptId}>
+            {option.pt.term}
+          </li>
+        )}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label={title}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {isLoading ? <CircularProgress size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          />
+        )}
+        disabled={isDisabled}
+      />
+    </div>
   );
 };
 
