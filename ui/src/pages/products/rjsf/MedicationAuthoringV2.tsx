aa952de17f76dad82bfa2db50c33f43448fbcd05
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2025-01-09 10:41:59 +1000

    Latest commit to switch to def

--- ui/src/pages/products/rjsf/MedicationAuthoringV2.tsx
+++ ui/src/pages/products/rjsf/MedicationAuthoringV2.tsx
@@ -1,44 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import './App.css';
-import {Form} from '@rjsf/mui';
-import {Container} from "@mui/material";
+import { Form } from '@rjsf/mui';
+import { Container } from '@mui/material';
 import validator from '@rjsf/validator-ajv8';
 
-import schema from "./MedicationProductDetails-schema.json";
-import uiSchema from "./MedicationProductDetails-uiSchema.json";
+import schema from './MedicationProductDetails-schema.json';
+import uiSchema from './MedicationProductDetails-uiSchema.json';
 
-import UnitValueField from "./UnitValueField.tsx";
-import AutoCompleteField from "./AutoCompleteField.tsx";
+import UnitValueField from './UnitValueField.tsx';
+import AutoCompleteField from './AutoCompleteField.tsx';
+import productService from '../../../api/ProductService.ts';
+import { isValueSetExpansionContains } from '../../../types/predicates/isValueSetExpansionContains.ts';
 
+import { Concept } from '../../../types/concept.ts';
+import type { ValueSetExpansionContains } from 'fhir/r4';
+import ProductLoader from '../components/ProductLoader.tsx';
+import ParentChildAutoCompleteField from "./ParentChildAutoCompleteField.tsx";
 
+export interface MedicationAuthoringV2Props {
+  selectedProduct: Concept | ValueSetExpansionContains | null;
+}
 
+function MedicationAuthoringV2({
+  selectedProduct,
+}: MedicationAuthoringV2Props) {
+  const [isLoadingProduct, setLoadingProduct] = useState(false);
+  const [formData, setFormData] = useState({});
 
+  useEffect(() => {
+    if (selectedProduct) {
+      setLoadingProduct(true);
+      productService
+        .fetchMedication(
+          selectedProduct
+            ? isValueSetExpansionContains(selectedProduct)
+              ? (selectedProduct.code as string)
+              : (selectedProduct.conceptId as string)
+            : '',
+          'MAIN',
+        )
+        .then(mp => {
+          if (mp.productName) {
+            setFormData(mp);
+            setLoadingProduct(false);
+          }
+        })
+        .catch(err => {
+          setLoadingProduct(false);
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedProduct]);
 
-function MedicationAuthoringV2() {
-    const handleSubmit = ({ formData }: any) => {
-        console.log("Submitted data:", formData);
-    };
+  const handleChange = ({ formData }) => {
+    setFormData(formData);
+  };
 
-
-    const log = (type) => console.log.bind(console, type);
-
-    return (
-        <Container>
-            <Form
-                schema={schema}
-                uiSchema={uiSchema}
-                fields={{ UnitValueField,AutoCompleteField}}
-                widgets={{ }}
-                formData={{}}
-                onChange={({ formData }) => console.log("Changed:", formData)} // Log changes
-                onSubmit={({ formData }) => console.log("Submitted:", formData)} // Log submission
-                onError={(errors) => console.log("Errors:", errors)} // Log errors
-                validator={validator}
-
-            />
-
-        </Container>
-    );
+  const log = type => console.log.bind(console, type);
+  if (isLoadingProduct) {
+    return <ProductLoader message={'Loading Product details'} />;
+  }
+  return (
+    <Container>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        onChange={handleChange}
+        fields={{ UnitValueField, AutoCompleteField,ParentChildAutoCompleteField }}
+        widgets={{}}
+        formData={formData}
+        onSubmit={({ formData }) => console.log('Submitted:', formData)} // Log submission
+        onError={errors => console.log('Errors:', errors)} // Log errors
+        validator={validator}
+      />
+    </Container>
+  );
 }
 
 export default MedicationAuthoringV2;
--- ui/src/pages/products/rjsf/SectionWidget.tsx
+++ ui/src/pages/products/rjsf/SectionWidget.tsx
@@ -1,21 +1,23 @@
-import React from "react";
-import { WidgetProps } from "@rjsf/core";
-import { Typography, Grid } from "@mui/material";
+import React from 'react';
+import { WidgetProps } from '@rjsf/core';
+import { Typography, Grid } from '@mui/material';
 
 const SectionWidget = ({ schema, uiSchema, children }: WidgetProps) => {
-    const title = uiSchema["ui:title"] || "Section"; // Default title if no title is provided
+  const title = uiSchema['ui:title'] || 'Section'; // Default title if no title is provided
 
-    return (
-        <div style={{ marginBottom: "20px" }}>
-            {/* Section Title */}
-            <Typography variant="h6" gutterBottom>{title}</Typography>
+  return (
+    <div style={{ marginBottom: '20px' }}>
+      {/* Section Title */}
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
 
-            {/* Wrap fields inside Grid for layout */}
-            <Grid container spacing={2}>
-                {children} {/* Render the fields inside the section */}
-            </Grid>
-        </div>
-    );
+      {/* Wrap fields inside Grid for layout */}
+      <Grid container spacing={2}>
+        {children} {/* Render the fields inside the section */}
+      </Grid>
+    </div>
+  );
 };
 
 export default SectionWidget;
