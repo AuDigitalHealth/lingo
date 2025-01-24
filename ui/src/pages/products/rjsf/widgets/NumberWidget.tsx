1c46a80dc1afb60e2d6fd94d0c1c5d0af3948cca
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2025-01-24 10:20:09 +1000

    saving changes

--- ui/src/pages/products/rjsf/widgets/NumberWidget.tsx
+++ ui/src/pages/products/rjsf/widgets/NumberWidget.tsx
@@ -1,22 +1,79 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import {
+    getTemplate,
+    getUiOptions,
+    RJSFSchema,
+    TemplatesType,
 
-const NumberWidget = ({ value, onChange, required, disabled, readonly, label, schema }) => {
+    WidgetProps,
+} from "@rjsf/utils";
+import { TextField } from "@mui/material";
+
+function NumberWidget<T = any, F extends RJSFSchema = RJSFSchema>({
+                                                                      id,
+                                                                      placeholder,
+                                                                      required,
+                                                                      readonly,
+                                                                      disabled,
+                                                                      label,
+                                                                      name,
+                                                                      value,
+                                                                      onChange,
+                                                                      onBlur,
+                                                                      onFocus,
+                                                                      autofocus,
+                                                                      options,
+                                                                      schema,
+                                                                      uiSchema,
+                                                                      rawErrors,
+                                                                      registry,
+                                                                      hideError,
+                                                                  }: WidgetProps<T, F>) {
+    const { widgets, templates } = registry;
+    const { title = "" } = getUiOptions<T, F>(uiSchema);
+    const BaseInputTemplate = getTemplate<"BaseInputTemplate", T, F>(
+        "BaseInputTemplate",
+        registry,
+        templates
+    );
+    const handleChange = React.useCallback(
+        (nextValue: string) =>
+            onChange(nextValue === "" ? "" : nextValue),
+        [onChange]
+    );
+    const handleBlur = React.useCallback(
+        ({ target: { value } }: React.FocusEvent<HTMLInputElement>) =>
+            onBlur(id, value),
+        [onBlur, id]
+    );
+    const handleFocus = React.useCallback(
+        ({ target: { value } }: React.FocusEvent<HTMLInputElement>) =>
+            onFocus(id, value),
+        [onFocus, id]
+    );
+
     return (
         <TextField
-            type="number"
-            label={label || schema.title}
-            value={value || ''}
-            onChange={(event) => {
-                const val = event.target.value;
-                onChange(val ? Number(val) : undefined);
-            }}
+            key={id}
+            id={id}
+            label={label}
+            name={name}
+            placeholder={placeholder}
+            value={value || value === 0 ? value : ""}
             required={required}
-            disabled={disabled || readonly}
-            variant="outlined"
+            disabled={disabled}
+            readOnly={readonly}
+            autoFocus={autofocus}
+            type="number"
+            // list={schema.examples ? `${id}-list` : undefined}
+            {...options}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            error={!!rawErrors?.length && !hideError}
+            helperText={!!rawErrors?.length && !hideError ? rawErrors.join(", ") : undefined}
             fullWidth
         />
     );
-};
-
+}
 export default NumberWidget;
