4f011cc3e4c4e9d5d3a3cec8687bcfe67fcc07e7
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2025-02-04 11:51:26 +1000

    applied header and ran prettier

--- ui/src/pages/products/rjsf/widgets/TextFieldWidget.tsx
+++ ui/src/pages/products/rjsf/widgets/TextFieldWidget.tsx
@@ -3,32 +3,32 @@ import { WidgetProps } from '@rjsf/core';
 import { TextField, Box } from '@mui/material';
 
 const TextFieldWidget = ({
-                                      id,
-                                      value,
-                                      required,
-                                      onChange,
-                                      rawErrors = [],
-                                      label,
-                                      options,
-                                  }: WidgetProps) => {
-    const placeholder = options?.placeholder || 'Enter text';
+  id,
+  value,
+  required,
+  onChange,
+  rawErrors = [],
+  label,
+  options,
+}: WidgetProps) => {
+  const placeholder = options?.placeholder || 'Enter text';
 
-    return (
-        <Box sx={{ width: '100%' }}>
-            <TextField
-                id={id}
-                fullWidth
-                variant="outlined"
-                placeholder={placeholder}
-                value={value || ''} // Controlled component
-                onChange={(e) => onChange(e.target.value)} // Pass the updated value
-                error={rawErrors.length > 0}
-                helperText={rawErrors.length > 0 ? rawErrors[0] : ''}
-                label={label}
-                required={required}
-            />
-        </Box>
-    );
+  return (
+    <Box sx={{ width: '100%' }}>
+      <TextField
+        id={id}
+        fullWidth
+        variant="outlined"
+        placeholder={placeholder}
+        value={value || ''} // Controlled component
+        onChange={e => onChange(e.target.value)} // Pass the updated value
+        error={rawErrors.length > 0}
+        helperText={rawErrors.length > 0 ? rawErrors[0] : ''}
+        label={label}
+        required={required}
+      />
+    </Box>
+  );
 };
 
 export default TextFieldWidget;
