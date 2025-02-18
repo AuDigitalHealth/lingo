95e94ddc659fccc2382091ee6d6f3a4f07be36e7
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2025-02-18 10:43:51 +1000

    added generation of schema and UI schema for identifiers

---
+++ api/src/main/java/au/gov/digitalhealth/lingo/configuration/model/validation/DefaultMappingTypeValidator.java
@@ -0,0 +1,17 @@
+package au.gov.digitalhealth.lingo.configuration.model.validation;
+
+import au.gov.digitalhealth.lingo.configuration.model.MappingRefset;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class DefaultMappingTypeValidator
+    implements ConstraintValidator<ValidDefaultMappingType, MappingRefset> {
+
+  @Override
+  public boolean isValid(MappingRefset mappingRefset, ConstraintValidatorContext context) {
+    if (mappingRefset.getDefaultMappingType() == null) {
+      return true; // If defaultMappingType is not set, no validation needed
+    }
+    return mappingRefset.getMappingTypes().contains(mappingRefset.getDefaultMappingType());
+  }
+}
---
+++ api/src/main/java/au/gov/digitalhealth/lingo/configuration/model/validation/ValidDefaultMappingType.java
@@ -0,0 +1,17 @@
+package au.gov.digitalhealth.lingo.configuration.model.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = DefaultMappingTypeValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidDefaultMappingType {
+  String message() default "defaultMappingType must be one of the mappingTypes";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
+}
