58bb8b9c1f9c94e425102722e2220b01eaa9780d
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2025-03-10 14:29:28 +1000

    Save bulk brand

--- ui/src/pages/products/rjsf/helpers/helpers.ts
+++ ui/src/pages/products/rjsf/helpers/helpers.ts
@@ -4,9 +4,12 @@ export const getItemTitle = (
   uiSchema: any,
   formData: any,
   index: number,
-  defaultTitle = 'Item',
+  defaultTitle = 'Untitled',
 ) => {
   const titleSource = uiSchema.items?.['ui:options']?.titleSource;
+  if(uiSchema.items?.['ui:options']?.defaultTitle){
+    defaultTitle = uiSchema.items?.['ui:options']?.defaultTitle;
+  }
   return titleSource && formData && formData[index]
     ? _.get(formData[index], titleSource) || `${defaultTitle} ${index + 1}`
     : `${defaultTitle} ${index + 1}`;
