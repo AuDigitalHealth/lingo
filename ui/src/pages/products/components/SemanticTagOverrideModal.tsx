a7ff694d79d5a975e0aa39e9629bfc19d56dff9d
Clinton Gillespie <clintongillespie@outlook.com>
2025-03-18 09:27:10 +1000

    added warning to 7 box preview screen if someone changes a semantic tag (#1449)

---
+++ ui/src/pages/products/components/SemanticTagOverrideModal.tsx
@@ -0,0 +1,61 @@
+import { Button, Stack, useTheme, Typography } from '@mui/material';
+import BaseModal from '../../../components/modal/BaseModal';
+import BaseModalBody from '../../../components/modal/BaseModalBody';
+import BaseModalFooter from '../../../components/modal/BaseModalFooter';
+import BaseModalHeader from '../../../components/modal/BaseModalHeader';
+
+interface SemanticTagOverrideModalProps {
+  open: boolean;
+  messages?: string[];
+  handleClose: () => void;
+  ignore: () => void;
+}
+export function SemanticTagOverrideModal({
+  open,
+  handleClose,
+  ignore,
+  messages,
+}: SemanticTagOverrideModalProps) {
+  const theme = useTheme();
+
+  return (
+    <BaseModal open={open} handleClose={handleClose}>
+      <div style={{ backgroundColor: theme.palette.warning.light }}>
+        <BaseModalHeader title={'Warning!'} />
+      </div>
+      <BaseModalBody data-testid={'override-modal'}>
+        <>
+          'The follow changes were found from the generated semantic tags:'
+          {messages?.map(message => {
+            return <Typography variant="body1">{message}</Typography>;
+          })}
+        </>
+      </BaseModalBody>
+      <BaseModalFooter
+        startChildren={<></>}
+        endChildren={
+          <Stack direction="row" spacing={1}>
+            <Button
+              color="primary"
+              size="small"
+              variant="contained"
+              onClick={() => handleClose()}
+              data-testid={'back-button'}
+            >
+              {'Go Back'}
+            </Button>
+            <Button
+              color="warning"
+              size="small"
+              variant="contained"
+              onClick={() => ignore()}
+              data-testid={'warning-and-proceed-btn'}
+            >
+              {'Proceed'}
+            </Button>
+          </Stack>
+        }
+      />
+    </BaseModal>
+  );
+}
