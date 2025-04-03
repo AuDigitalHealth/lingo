017a192ab9144de380b48d8c7767e9c3f5464194
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2025-04-03 12:06:12 +1000

    Unwrapping this includes broken changes for multipack and device

--- common/src/main/java/au/gov/digitalhealth/lingo/product/details/PackageQuantity.java
+++ common/src/main/java/au/gov/digitalhealth/lingo/product/details/PackageQuantity.java
@@ -15,22 +15,23 @@
  */
 package au.gov.digitalhealth.lingo.product.details;
 
-import au.csiro.snowstorm_client.model.SnowstormConceptMini;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.util.Map;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = false)
-public class DeviceAndSpecificDeviceType extends ProductBaseDto {
+@EqualsAndHashCode(callSuper = true)
+public class PackageQuantity<T extends ProductDetails> extends Quantity {
+  @NotNull @Valid PackageDetails<T> packageDetails;
 
-    @NotNull SnowstormConceptMini deviceType;
-
-    SnowstormConceptMini specificDeviceType;
-
-    @Override
-    public Map<String, String> getIdFsnMap() {
-        return addToIdFsnMap(null, deviceType, specificDeviceType);
-    }
+  @Override
+  @JsonIgnore
+  public Map<String, String> getIdFsnMap() {
+    Map<String, String> idMap = packageDetails.getIdFsnMap();
+    idMap.putAll(super.getIdFsnMap());
+    return idMap;
+  }
 }
