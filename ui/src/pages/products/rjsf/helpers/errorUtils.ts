fd1e83f77ae270798246b95575060b2489b97fd1
lukeswindale <luke@swinmail.com>
2025-04-07 11:36:08 +1000

    Addressed linting issues for the rjsf work

--- ui/src/pages/products/rjsf/helpers/errorUtils.ts
+++ ui/src/pages/products/rjsf/helpers/errorUtils.ts
@@ -44,7 +44,7 @@ export const getUniqueErrors = (
 
 export const extractAllErrorMessages = (
   errorSchema: any,
-  parentPath: string = '',
+  parentPath: '',
 ): string[] => {
   let errors: string[] = [];
 
