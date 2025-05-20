1de962198a86b67423b8860356a818a495f9dbf9
lukeswindale <luke@swinmail.com>
2025-05-20 15:46:22 +1000

    refactored meds and device authoring to remove most of the custom components.

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -54,6 +54,7 @@ module.exports = {
         '@typescript-eslint/no-floating-promises': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/restrict-template-expressions': 'off',
         'react-hooks/exhaustive-deps': 'off',
         'react-refresh/only-export-components': 'off',
