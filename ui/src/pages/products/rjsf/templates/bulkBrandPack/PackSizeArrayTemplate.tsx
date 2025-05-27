3f9e391f98c51f4f0588dfe25dfd62f27cf8c6bb
lukeswindale <luke@swinmail.com>
2025-05-27 14:45:07 +1000

    Pushing the changes thus far for bulk pack size authoring. NOTE: This is still a work in progress. Am pushing purely to enable handover. Especially be aware that for PackSizeAuthoring.tsx, I've hardcoded the schema and uiSchema at the bottom of that file... that cannot stay!!

--- ui/src/pages/products/rjsf/templates/bulkBrandPack/PackSizeArrayTemplate.tsx
+++ ui/src/pages/products/rjsf/templates/bulkBrandPack/PackSizeArrayTemplate.tsx
@@ -2,101 +2,70 @@ import React from 'react';
 import { ArrayFieldTemplateProps } from '@rjsf/utils';
 import {
   Box,
-  IconButton,
   List,
   ListItem,
-  ListSubheader,
-  Tooltip,
-  Typography,
+  Typography
 } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import MedicationIcon from '@mui/icons-material/Medication';
-import { Avatar } from '@mui/material';
-import { FieldChips } from '../../../components/ArtgFieldChips.tsx';
-import { sortExternalIdentifiers } from '../../../../../utils/helpers/tickets/additionalFieldsUtils.ts';
+import PackDetails from '../../fields/bulkBrandPack/PackDetails.tsx';
 
-const PackSizeArrayTemplate: React.FC<ArrayFieldTemplateProps> = props => {
+const PackSizeArrayTemplate: React.FC<ArrayFieldTemplateProps> = (props) => {
   const { items, uiSchema, registry } = props;
   const { formContext } = registry;
 
   const options = uiSchema['ui:options'] || {};
   const listTitle = options.listTitle || 'Pack Sizes';
-  const isReadOnly = options.readOnly || false;
+  const isReadOnly = (!!options.readOnly) ?? true;
+  const allowDelete = (!!options.allowDelete) ?? true;
+  const requireEditButton = (!!options.requireEditButton) ?? true;
 
   const unitOfMeasure = formContext.formData?.unitOfMeasure;
 
-  const handleDelete = (index: number) => {
+  const [formData, setFormData] = React.useState(
+      props.formData || []);
+
+  const handleDeletePackSize = (packDetails: any) => {
     const currentFormData = { ...formContext.formData };
-    const fieldName =
-      uiSchema['ui:options'].title === 'Existing Pack Sizes'
-        ? 'existingPackSizes'
-        : 'packSizes';
-    const updatedPackSizes = [...(currentFormData[fieldName] || [])];
-    updatedPackSizes.splice(index, 1);
-    const updatedFormData = {
-      ...currentFormData,
-      [fieldName]: updatedPackSizes,
-    };
-    formContext.onChange(updatedFormData);
+    // @ts-ignore
+    currentFormData?.packSizes?.splice(currentFormData?.packSizes.indexOf(packDetails), 1);
+    formContext.onFormDataChange(currentFormData);
   };
 
   return (
-    <Box>
-      <List
-        subheader={<ListSubheader>{listTitle}</ListSubheader>}
-        sx={{ border: 1, borderColor: 'grey.300', borderRadius: 1, p: 1 }}
-      >
-        {items.length > 0 ? (
-          items.map((element, index) => (
-            <ListItem
-              key={index}
-              secondaryAction={
-                !isReadOnly &&
-                element.hasRemove && (
-                  <Tooltip title="Remove Pack Size">
-                    <IconButton
-                      edge="end"
-                      aria-label="delete"
-                      onClick={() => handleDelete(index)}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </Tooltip>
-                )
-              }
-              sx={{ alignItems: 'flex-start', py: 0.5 }} // Tighten vertical padding
-            >
-              <Avatar sx={{ mr: 1, alignSelf: 'center' }}>
-                <MedicationIcon />
-              </Avatar>
-              <Box display="flex" flexDirection="column" sx={{ width: '100%' }}>
-                <Box display="flex" alignItems="center">
-                  <Typography variant="body1" sx={{ mr: 1 }}>
-                    {element.children.props.formData.packSize || 'N/A'}
-                  </Typography>
-                  {unitOfMeasure && (
-                    <Typography variant="body2" color="textSecondary">
-                      {unitOfMeasure.pt.term}
-                    </Typography>
-                  )}
-                </Box>
-                <FieldChips
-                  items={sortExternalIdentifiers(
-                    element.children.props.formData.externalIdentifiers || [],
-                  )}
-                  sx={{ mt: 1 }}
-                />
-              </Box>
-            </ListItem>
-          ))
-        ) : (
-          <ListItem>
-            <Typography variant="body1">No pack sizes added</Typography>
-          </ListItem>
-        )}
-      </List>
-    </Box>
+      <Box>
+        <List sx={{ border: 1, borderColor: 'grey.300', borderRadius: 1, p: 1 }}
+        >
+          {items.length > 0 ? (
+              items.map((element, index) => (
+                  <PackDetails
+                      {...element.children.props}
+                      key={index}
+                      index={index}
+                      unitOfMeasure={unitOfMeasure}
+                      onDelete={handleDeletePackSize}
+                      uiSchema={{
+                        'ui:options': {
+                          readOnly: isReadOnly,
+                          allowDelete: allowDelete,
+                          requireEditButton: requireEditButton
+                        }
+                      }}
+                      sx={{
+                        '& .MuiListItemSecondaryAction-root': {
+                          top: '20px'
+                        },
+                      }}
+                  />
+              ))
+          ) : (
+              <ListItem>
+                <Typography variant="body1">No pack sizes added</Typography>
+              </ListItem>
+          )}
+        </List>
+      </Box>
   );
 };
 
 export default PackSizeArrayTemplate;
+
+
