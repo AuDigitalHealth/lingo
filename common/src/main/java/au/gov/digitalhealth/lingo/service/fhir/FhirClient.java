3027828c8fe53b9f288021a8e227c17b8830e41d
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2025-05-30 12:49:41 +1000

    Added FHIR client integration for resolving CODED external identifiers
    Integrated FHIR client for resolving CODED external identifiers in mapping refsets. Updated relevant methods, services, and model configurations to support this functionality. Enhanced validation rules for model configurations with CODED and CONCEPT types.

---
+++ common/src/main/java/au/gov/digitalhealth/lingo/service/fhir/FhirClient.java
@@ -0,0 +1,80 @@
+package au.gov.digitalhealth.lingo.service.fhir;
+
+import au.csiro.snowstorm_client.model.SnowstormConceptMini;
+import au.csiro.snowstorm_client.model.SnowstormTermLangPojo;
+import au.gov.digitalhealth.lingo.service.fhir.FhirParameters.Parameter;
+import au.gov.digitalhealth.lingo.service.fhir.FhirParameters.Parameter.Part;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class FhirClient {
+  private final WebClient client;
+
+  public FhirClient(@Qualifier("fhirApiClient") WebClient client) {
+    this.client = client;
+  }
+
+  private static boolean isInActive(FhirParameters parameters) {
+    return parameters.getParameter().stream()
+        .filter(param -> "property".equals(param.getName()))
+        .filter(
+            param ->
+                param.getPart().stream()
+                    .anyMatch(
+                        part ->
+                            "inactive".equals(part.getValueCode())
+                                && "code".equals(part.getName())))
+        .flatMap(param -> param.getPart().stream())
+        .filter(part -> "value".equals(part.getName()))
+        .map(Part::getValueBoolean)
+        .findFirst()
+        .orElseThrow(() -> new IllegalStateException("No active status found in FHIR parameters"));
+  }
+
+  private static String getDisplayName(FhirParameters parameters) {
+    return parameters.getParameter().stream()
+        .filter(param -> "display".equals(param.getName()))
+        .map(Parameter::getValueString)
+        .findFirst()
+        .orElseThrow(() -> new IllegalStateException("No display name found in FHIR parameters"));
+  }
+
+  public Mono<SnowstormConceptMini> getConcept(String code, String system) {
+    if (code == null || code.isEmpty()) {
+      return Mono.error(new IllegalArgumentException("Code must not be null or empty"));
+    }
+
+    if (system == null || system.isEmpty()) {
+      return Mono.error(new IllegalArgumentException("System must not be null or empty"));
+    }
+
+    return client
+        .get()
+        .uri(
+            uriBuilder ->
+                uriBuilder
+                    .path("/CodeSystem/$lookup")
+                    .queryParam("code", code)
+                    .queryParam("system", system)
+                    .build())
+        .retrieve()
+        .bodyToMono(FhirParameters.class)
+        .map(
+            parameters -> {
+              SnowstormConceptMini snowstormConceptMini = new SnowstormConceptMini();
+              snowstormConceptMini.setConceptId(code);
+              snowstormConceptMini.setId(code);
+              snowstormConceptMini.setActive(!isInActive(parameters));
+              SnowstormTermLangPojo pt =
+                  new SnowstormTermLangPojo().lang("en").term(getDisplayName(parameters));
+              snowstormConceptMini.setPt(pt);
+              SnowstormTermLangPojo fsn =
+                  new SnowstormTermLangPojo().lang("en").term(getDisplayName(parameters));
+              snowstormConceptMini.setFsn(fsn);
+              return snowstormConceptMini;
+            });
+  }
+}
