8537b10d56d86250d53f24e140f2369193675779
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2025-06-12 23:07:21 +1000

    added in fire properties to 6 box model rendering

--- common/src/main/java/au/gov/digitalhealth/lingo/service/fhir/FhirParameters.java
+++ common/src/main/java/au/gov/digitalhealth/lingo/service/fhir/FhirParameters.java
@@ -1,6 +1,8 @@
 package au.gov.digitalhealth.lingo.service.fhir;
 
+import au.gov.digitalhealth.lingo.service.fhir.FhirParameters.Parameter.Part.Coding;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.math.BigDecimal;
 import java.util.List;
 import lombok.Data;
 
@@ -23,14 +25,79 @@ public class FhirParameters {
     @JsonProperty("valueString")
     private String valueString;
 
-    @JsonProperty("valueUri")
-    private String valueUri;
-
     @JsonProperty("valueBoolean")
     private Boolean valueBoolean;
 
+    @JsonProperty("valueCoding")
+    private Coding valueCoding;
+
+    @JsonProperty("valueInteger")
+    private Integer valueInteger;
+
+    @JsonProperty("valueDecimal")
+    private BigDecimal valueDecimal;
+
+    @JsonProperty("valueDateTime")
+    private String valueDateTime;
+
+    @JsonProperty("valueUri")
+    private String valueUri;
+
     @JsonProperty("part")
     private List<Part> part;
+
+    public boolean hasValueString() {
+      return valueString != null;
+    }
+
+    public boolean hasValueCode() {
+      return valueCode != null;
+    }
+
+    public boolean hasValueBoolean() {
+      return valueBoolean != null;
+    }
+
+    public boolean hasValueInteger() {
+      return valueInteger != null;
+    }
+
+    public boolean hasValueDecimal() {
+      return valueDecimal != null;
+    }
+
+    public boolean hasValueDateTime() {
+      return valueDateTime != null;
+    }
+
+    public boolean hasValueUri() {
+      return valueUri != null;
+    }
+
+    public boolean hasValueCoding() {
+      return valueCoding != null;
+    }
+
+    public String getValueAsString() {
+      if (hasValueString()) {
+        return valueString;
+      } else if (hasValueCode()) {
+        return valueCode;
+      } else if (hasValueBoolean()) {
+        return valueBoolean.toString();
+      } else if (hasValueInteger()) {
+        return valueInteger.toString();
+      } else if (hasValueDecimal()) {
+        return valueDecimal.toString();
+      } else if (hasValueDateTime()) {
+        return valueDateTime;
+      } else if (hasValueUri()) {
+        return valueUri;
+      } else if (hasValueCoding()) {
+        return valueCoding.getCode();
+      }
+      return null;
+    }
 
     @Data
     public static class Part {
@@ -48,6 +115,71 @@ public class FhirParameters {
 
       @JsonProperty("valueCoding")
       private Coding valueCoding;
+
+      @JsonProperty("valueInteger")
+      private Integer valueInteger;
+
+      @JsonProperty("valueDecimal")
+      private BigDecimal valueDecimal;
+
+      @JsonProperty("valueDateTime")
+      private String valueDateTime;
+
+      @JsonProperty("valueUri")
+      private String valueUri;
+
+      public boolean hasValueString() {
+        return valueString != null;
+      }
+
+      public boolean hasValueCode() {
+        return valueCode != null;
+      }
+
+      public boolean hasValueBoolean() {
+        return valueBoolean != null;
+      }
+
+      public boolean hasValueInteger() {
+        return valueInteger != null;
+      }
+
+      public boolean hasValueDecimal() {
+        return valueDecimal != null;
+      }
+
+      public boolean hasValueDateTime() {
+        return valueDateTime != null;
+      }
+
+      public boolean hasValueUri() {
+        return valueUri != null;
+      }
+
+      public boolean hasValueCoding() {
+        return valueCoding != null;
+      }
+
+      public String getValueAsString() {
+        if (hasValueString()) {
+          return valueString;
+        } else if (hasValueCode()) {
+          return valueCode;
+        } else if (hasValueBoolean()) {
+          return valueBoolean.toString();
+        } else if (hasValueInteger()) {
+          return valueInteger.toString();
+        } else if (hasValueDecimal()) {
+          return valueDecimal.toString();
+        } else if (hasValueDateTime()) {
+          return valueDateTime;
+        } else if (hasValueUri()) {
+          return valueUri;
+        } else if (hasValueCoding()) {
+          return valueCoding.getCode();
+        }
+        return null;
+      }
 
       @Data
       public static class Coding {
