9c5b0943ca7f30c44df7bc4a811cbe2b825a1f17
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2025-10-24 13:48:24 +1000

    Fix bugs related to the external identifiers

--- common/src/main/java/au/gov/digitalhealth/lingo/product/details/properties/FieldValue.java
+++ common/src/main/java/au/gov/digitalhealth/lingo/product/details/properties/FieldValue.java
@@ -16,6 +16,7 @@
 package au.gov.digitalhealth.lingo.product.details.properties;
 
 import au.csiro.snowstorm_client.model.SnowstormConceptMini;
+import java.io.Serializable;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,7 +27,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class FieldValue {
+public class FieldValue implements Serializable {
   String value;
   SnowstormConceptMini valueObject;
   String codeSystem;
